332351d294fd94acf78e947bae91bd7d7d143132
Brieuc Viel <brieuc.viel@belledonne-communications.com>
2017-04-07 10:48:56 +0200

    [JNI] remove listener unregistration on LinphoneMessageState = NotDelivered

--- coreapi/linphonecore_jni.cc
+++ coreapi/linphonecore_jni.cc
@@ -4544,6 +4544,12 @@ extern "C" jboolean Java_org_linphone_core_LinphoneChatMessageImpl_isSecured(JNI
 	return linphone_chat_message_is_secured((LinphoneChatMessage*)ptr);
 }
 
+extern "C" void Java_org_linphone_core_LinphoneChatMessageImpl_reSend(JNIEnv*  env
+																		,jobject  thiz
+																		,jlong ptr) {
+	linphone_chat_message_resend((LinphoneChatMessage*)ptr);
+}
+
 static void message_state_changed(LinphoneChatMessage* msg, LinphoneChatMessageState state) {
 	JNIEnv *env = 0;
 	jint result = jvm->AttachCurrentThread(&env,NULL);
@@ -4568,7 +4574,7 @@ static void message_state_changed(LinphoneChatMessage* msg, LinphoneChatMessageS
 	LinphoneJavaBindings *ljb = (LinphoneJavaBindings *)linphone_core_get_user_data(lc);
 	env->CallVoidMethod(listener, method, jmessage, env->CallStaticObjectMethod(ljb->chatMessageStateClass, ljb->chatMessageStateFromIntId, (jint)state));
 
-	if (state == LinphoneChatMessageStateDisplayed || state == LinphoneChatMessageStateNotDelivered) {
+	if (state == LinphoneChatMessageStateDisplayed) {
 		env->DeleteGlobalRef(listener);
 		msg->message_state_changed_user_data = NULL;
 	}
@@ -4731,7 +4737,7 @@ static void chat_room_impl_callback(LinphoneChatMessage* msg, LinphoneChatMessag
 			jmessage,
 			env->CallStaticObjectMethod(ljb->chatMessageStateClass,ljb->chatMessageStateFromIntId,(jint)state));
 
-	if (state == LinphoneChatMessageStateDisplayed || state == LinphoneChatMessageStateNotDelivered) {
+	if (state == LinphoneChatMessageStateDisplayed ) {
 		env->DeleteGlobalRef(listener);
 		env->DeleteGlobalRef(jmessage);
 		linphone_chat_message_set_user_data(msg,NULL);
