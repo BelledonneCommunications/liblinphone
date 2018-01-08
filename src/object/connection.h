053c0976210f7d8cc047f92c6a8a4716d732ad81
Ronan Abhamon <ronan.abhamon@belledonne-communications.com>
2018-01-08 16:58:25 +0100

    feat(Object): provide a connection object

---
+++ src/object/connection.h
@@ -0,0 +1,47 @@
+/*
+ * connection.h
+ * Copyright (C) 2010-2017 Belledonne Communications SARL
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef _L_CONNECTION_H_
+#define _L_CONNECTION_H_
+
+#include "linphone/utils/general.h"
+
+// =============================================================================
+
+LINPHONE_BEGIN_NAMESPACE
+
+class Connection {
+public:
+	Connection () = default;
+	~Connection () = default;
+
+	operator bool () const {
+		return mConnected;
+	}
+
+private:
+	// Object *mSender = nullptr;
+	// Object *mReceiver = nullptr;
+
+	bool mConnected = false;
+};
+
+LINPHONE_END_NAMESPACE
+
+#endif // ifndef _L_CONNECTION_H_
