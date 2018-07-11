bfbfb3c1746fdd8e15ac5587b40feec3074a711c
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2018-07-11 16:42:16 +0200

    Fix to be able to use AAR in 3.4.X android branch

--- java/impl/org/linphone/core/tools/AndroidPlatformHelper.java
+++ java/impl/org/linphone/core/tools/AndroidPlatformHelper.java
@@ -18,7 +18,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 
-package org.linphone.tools;
+package org.linphone.core.tools;
 
 import org.linphone.mediastream.Log;
 import org.linphone.mediastream.MediastreamerAndroidContext;
