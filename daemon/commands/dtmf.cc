9dd67b5522ed2b5d494aca978019accbb643305c
Jacob Floyd <cognifloyd@gmail.com>
2018-08-03 12:12:47 -0500

    Fix dtmf in daemon and align with linphone-desktop
    This was updated to use a newer dtmf api in [1] (where it moved from
    core to call), but the check for an active call is actually ensuring
    that dtmf is only sent if there are no active calls. When there are no
    active calls, the daemon displays this error::
    
      ortp-error-No dtmf generator at this time !
    
    Also, in comparing the dtmf logic in deamon and desktop, the dtmf tone
    plays out for longer in desktop, so use that[2][3].
    
    [1] https://github.com/BelledonneCommunications/linphone/commit/af2ecd8cb67845e904d803d008ced2703e45423e
    [2] https://github.com/BelledonneCommunications/linphone-desktop/blob/a3dc41ec5582276d4ee93b7c4cf193842c03dc7b/src/components/call/CallModel.cpp#L581-L586
    [3] https://github.com/BelledonneCommunications/linphone-desktop/commit/af98291fac4be80cbd6a9a61987403d112b9cb57

--- daemon/commands/dtmf.cc
+++ daemon/commands/dtmf.cc
@@ -44,8 +44,8 @@ void DtmfCommand::exec(Daemon *app, const string& args) {
 	digit = digit_str.at(0);
 	if (isdigit(digit) || (digit == 'A') || (digit == 'B') || (digit == 'C') || (digit == 'D') || (digit == '*') || (digit == '#')) {
 		LinphoneCall *call = linphone_core_get_current_call(app->getCore());
-		linphone_core_play_dtmf(app->getCore(), digit, 100);
-		if (call == NULL) {
+		linphone_core_play_dtmf(app->getCore(), digit, 200);
+		if (call != NULL) {
 			linphone_call_send_dtmf(call, digit);
 		}
 		app->sendResponse(Response());
