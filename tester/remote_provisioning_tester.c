0c389c34a82cc76babb617a65b06b67b78b0dd1d
Jehan Monnier <jehan.monnier@linphone.org>
2018-09-21 14:19:43 +0200

    clear proxy config before loading config from config.

--- tester/remote_provisioning_tester.c
+++ tester/remote_provisioning_tester.c
@@ -45,6 +45,8 @@ static void remote_provisioning_http(void) {
 	LinphoneCoreManager* marie = linphone_core_manager_new2("marie_remote_rc", FALSE);
 	BC_ASSERT_TRUE(wait_for(marie->lc,NULL,&marie->stat.number_of_LinphoneConfiguringSuccessful,1));
 	BC_ASSERT_TRUE(wait_for(marie->lc,NULL,&marie->stat.number_of_LinphoneRegistrationOk,1));
+	/*make sure proxy config is not added in double, one time at core init, next time at configuring successfull*/
+	BC_ASSERT_EQUAL(bctbx_list_size(linphone_core_get_proxy_config_list(marie->lc)), 1, int,"%i");
 	linphone_core_manager_destroy(marie);
 }
 
