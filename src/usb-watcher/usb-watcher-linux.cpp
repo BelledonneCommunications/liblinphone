7ae5d8bb2723aabbf8224f0f89d30ad79998f8f6
Ronan Abhamon <ronan.abhamon@belledonne-communications.com>
2018-09-25 11:50:19 +0200

    in progress

---
+++ src/usb-watcher/usb-watcher-linux.cpp
@@ -0,0 +1,85 @@
+/*
+ * usb-watcher-linux.h
+ * Copyright (C) 2010-2018 Belledonne Communications SARL
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef _L_USB_WATCHER_LINUX_H_
+#define _L_USB_WATCHER_LINUX_H_
+
+#include <libudev.h>
+
+#include "logger/logger.h"
+#include "object/object-p.h"
+#include "usb-watcher.h"
+
+// =============================================================================
+
+LINPHONE_BEGIN_NAMESPACE
+
+class UsbWatcherPrivate : public ObjectPrivate {
+public:
+	udev *udevContext;
+	udev_monitor *udevMonitor;
+
+	void clean ();
+};
+
+void UsbWatcherPrivate::clean () {
+	if (udevMonitor)
+		udev_monitor_unref(udevMonitor);
+	if (udevContext)
+		udev_unref(udevContext);
+}
+
+UsbWatcher::UsbWatcher () : Object(*new UsbWatcherPrivate) {
+	L_D();
+
+	if (!(d->udevContext = udev_new())) {
+		lError() << "Unable to create udev context.";
+		return;
+	}
+
+	if (!(d->udevMonitor = udev_monitor_new_from_netlink(d->udevContext, "udev"))) {
+		lError() << "Unable to create udev monitor.";
+		d->clean();
+		return;
+	}
+
+	for (const char *subsystem : { "sound", "video4linux" })
+		if (udev_monitor_filter_add_match_subsystem_devtype(d->udevMonitor, subsystem, nullptr) < 0) {
+			lError() << "Unable to monitor: " << subsystem;
+			d->clean();
+			return;
+		}
+
+	int fd = udev_monitor_get_fd(d->udevMonitor);
+	if (fd < 0) {
+		d->clean();
+		return;
+	}
+
+	// TODO: Deal with fd. Add a socket notifier in core?
+}
+
+UsbWatcher::~UsbWatcher () {
+	L_D();
+	d->clean();
+}
+
+LINPHONE_END_NAMESPACE
+
+#endif // ifndef _L_USB_WATCHER_LINUX_H_
---
+++ src/usb-watcher/usb-watcher.h
@@ -0,0 +1,43 @@
+/*
+ * usb-watcher.h
+ * Copyright (C) 2010-2018 Belledonne Communications SARL
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef _L_USB_WATCHER_H_
+#define _L_USB_WATCHER_H_
+
+#include "object/object.h"
+
+// =============================================================================
+
+LINPHONE_BEGIN_NAMESPACE
+
+class UsbWatcherPrivate;
+
+class UsbWatcher : public Object {
+public:
+	UsbWatcher ();
+	~UsbWatcher ();
+
+private:
+	L_DECLARE_PRIVATE(UsbWatcher);
+	L_DISABLE_COPY(UsbWatcher);
+};
+
+LINPHONE_END_NAMESPACE
+
+#endif // ifndef _L_USB_WATCHER_H_
