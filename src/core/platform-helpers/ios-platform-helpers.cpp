d4676fb9d40304336229b38d4444b858b2550543
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2018-10-16 14:15:19 +0200

    Include rootca.pem in the iOS framework.

--- src/core/platform-helpers/ios-platform-helpers.cpp
+++ src/core/platform-helpers/ios-platform-helpers.cpp
@@ -71,21 +71,27 @@ IosPlatformHelpers::IosPlatformHelpers (LinphoneCore *lc, void *system_context) 
 	mCpuLockCount = 0;
 	mCpuLockTaskId = 0;
 
-	std::string cpimPath = directoryForResource(CFSTR("org.linphone.linphone"), CFSTR("cpim_grammar"));
+	string rootCaPath = directoryForResource(CFSTR("org.linphone.linphone"), CFSTR("rootca.pem"));
+	if (!rootCaPath.empty())
+		linphone_core_set_root_ca(lc, rootCaPath.c_str());
+	else
+		lError() << "IosPlatformHelpers did not find rootca.pem resource";
+
+	string cpimPath = directoryForResource(CFSTR("org.linphone.linphone"), CFSTR("cpim_grammar"));
 	if (!cpimPath.empty())
 		belr::GrammarLoader::get().addPath(cpimPath);
 	else
 		lError() << "IosPlatformHelpers did not find cpim grammar resource directory...";
 
 #ifdef VCARD_ENABLED
-	std::string vcardPath = directoryForResource(CFSTR("org.linphone.belcard"), CFSTR("vcard_grammar"));
+	string vcardPath = directoryForResource(CFSTR("org.linphone.belcard"), CFSTR("vcard_grammar"));
 	if (!vcardPath.empty())
 		belr::GrammarLoader::get().addPath(vcardPath);
 	else
 		lError() << "IosPlatformHelpers did not find vcard grammar resource directory...";
 #endif
 
-	lInfo() << "IosPlatformHelpers is fully initialised.";
+	lInfo() << "IosPlatformHelpers is fully initialised";
 }
 
 // -----------------------------------------------------------------------------
@@ -127,16 +133,16 @@ void IosPlatformHelpers::releaseCpuLock () {
 	mCpuLockTaskId = 0;
 }
 
-std::string IosPlatformHelpers::directoryForResource (CFStringRef framework, CFStringRef resource) {
+string IosPlatformHelpers::directoryForResource (CFStringRef framework, CFStringRef resource) {
 	CFBundleRef bundle = CFBundleGetBundleWithIdentifier(framework);
-	CFURLRef grammarUrl = CFBundleCopyResourceURL(bundle, resource, nullptr, nullptr);
-	CFURLRef grammarUrlDirectory = CFURLCreateCopyDeletingLastPathComponent(nullptr, grammarUrl);
-	CFStringRef grammarPath = CFURLCopyFileSystemPath(grammarUrlDirectory, kCFURLPOSIXPathStyle);
+	CFURLRef resourceUrl = CFBundleCopyResourceURL(bundle, resource, nullptr, nullptr);
+	CFURLRef resourceUrlDirectory = CFURLCreateCopyDeletingLastPathComponent(nullptr, resourceUrl);
+	CFStringRef resourcePath = CFURLCopyFileSystemPath(resourceUrlDirectory, kCFURLPOSIXPathStyle);
 	CFStringEncoding encodingMethod = CFStringGetSystemEncoding();
-	std::string path(CFStringGetCStringPtr(grammarPath, encodingMethod));
-	CFRelease(grammarUrl);
-	CFRelease(grammarPath);
-	CFRelease(grammarUrlDirectory);
+	string path(CFStringGetCStringPtr(resourcePath, encodingMethod));
+	CFRelease(resourceUrl);
+	CFRelease(resourcePath);
+	CFRelease(resourceUrlDirectory);
 	return path;
 }
 
