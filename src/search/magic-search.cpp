ce7e61bb8a1866a5e6e8a1472ff0dd710fd7c310
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2018-12-21 13:33:19 +0100

    Fixed results list when there is no search

--- src/search/magic-search.cpp
+++ src/search/magic-search.cpp
@@ -347,14 +347,15 @@ list<SearchResult> MagicSearch::getFriends (const string &withDomain) const {
 }
 
 list<SearchResult> *MagicSearch::beginNewSearch (const string &filter, const string &withDomain) const {
+	list<SearchResult> returnList;
 	list<SearchResult> clResults;
-	list<SearchResult> *resultList = new list<SearchResult>();
-	LinphoneFriendList *fList = linphone_core_get_default_friend_list(this->getCore()->getCCore());
+	list<SearchResult> friendResults;
+	LinphoneFriendList *list = linphone_core_get_default_friend_list(this->getCore()->getCCore());
 
-	// For all friends or when we reach the search limit
-	for (bctbx_list_t *f = fList->friends ; f != nullptr ; f = bctbx_list_next(f)) {
-		list<SearchResult> fResults = searchInFriend(reinterpret_cast<LinphoneFriend*>(f->data), filter, withDomain);
-		addResultsToResultsList(fResults, *resultList);
+	for (bctbx_list_t *f = list->friends ; f != nullptr ; f = bctbx_list_next(f)) {
+		const LinphoneFriend *lFriend = reinterpret_cast<LinphoneFriend*>(f->data);
+		friendResults = searchInFriend(lFriend, "", withDomain);
+		addResultsToResultsList(friendResults, returnList);
 	}
 
 	clResults = getAddressFromCallLog(filter, withDomain, *resultList);
