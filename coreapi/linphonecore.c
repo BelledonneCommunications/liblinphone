1f26b17d2ce1abdfbac107e2259e4d72d15ab38a
Benjamin Reis <benjamin.reis@belledonne-communications.com>
2019-01-10 15:22:12 +0100

    fix build

--- coreapi/linphonecore.c
+++ coreapi/linphonecore.c
@@ -2287,7 +2287,7 @@ static void _linphone_core_conference_subscription_state_changed (LinphoneCore *
 
 		return;
 	}
-
+/*
 	const LinphoneAddress *resource = linphone_event_get_resource(lev);
 	shared_ptr<AbstractChatRoom> chatRoom = L_GET_CPP_PTR_FROM_C_OBJECT(lc)->findChatRoom(LinphonePrivate::ConferenceId(
 		IdentityAddress(*L_GET_CPP_PTR_FROM_C_OBJECT(resource)),
@@ -2295,6 +2295,7 @@ static void _linphone_core_conference_subscription_state_changed (LinphoneCore *
 	));
 	if (chatRoom)
 		L_GET_PRIVATE(static_pointer_cast<ServerGroupChatRoom>(chatRoom))->subscriptionStateChanged(lev, state);
+*/
 }
 
 static void linphone_core_internal_subscription_state_changed(LinphoneCore *lc, LinphoneEvent *lev, LinphoneSubscriptionState state) {
