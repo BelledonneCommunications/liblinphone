a3bc9e44591af50da286eb3665c15b3d618a6746
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2019-03-01 09:12:39 +0100

    Fixed old java wrapper compilation

--- wrappers/java/classes/tools/AndroidPlatformHelper.java
+++ wrappers/java/classes/tools/AndroidPlatformHelper.java
@@ -20,7 +20,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 package org.linphone.core.tools;
 
-import org.linphone.core.Core;
 import org.linphone.core.tools.DozeReceiver;
 import org.linphone.core.tools.NetworkManager;
 import org.linphone.core.tools.NetworkManagerAbove21;
