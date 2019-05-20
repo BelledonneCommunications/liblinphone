8e3bd2c865fbedd3f7e71f94564b6df654af497f
Danmei Chen <danmei.chen@belledonne-communications.com>
2019-05-20 15:02:17 +0200

    cmake build swift framework

---
+++ share/Modules/include.h
@@ -0,0 +1,24 @@
+//
+//  include.h
+//  LinphoneSdk
+//
+//  Created by Danmei Chen on 15/05/2019.
+//  Copyright © 2019 belledonne. All rights reserved.
+//
+
+#ifndef include_h
+#define include_h
+#import "linphone/factory.h"
+#import "linphone/types.h"
+
+#import "linphone/auth_info.h"
+#import "linphone/core.h"
+#import "linphone/tunnel.h"
+#import "linphone/wrapper_utils.h"
+#import "linphone/core_utils.h"
+
+#import "belle-sip/object.h"
+#import "bctoolbox/list.h"
+#import "mediastreamer2/msfactory.h"
+
+#endif /* include_h */
