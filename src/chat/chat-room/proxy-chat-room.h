4118f749be0c213fb5678877256c650026b66f6b
Danmei Chen <danmei.chen@belledonne-communications.com>
2019-09-05 13:06:15 +0200

    addContents override

--- src/chat/chat-room/proxy-chat-room.h
+++ src/chat/chat-room/proxy-chat-room.h
@@ -68,6 +68,7 @@ public:
 	std::shared_ptr<ChatMessage> createChatMessage (const std::string &text) override;
 
 	std::shared_ptr<ChatMessage> createFileTransferMessage (FileContent *content) override;
+	std::shared_ptr<ChatMessage> createForwardMessage (const std::shared_ptr<ChatMessage> &msg) override;
 
 	std::shared_ptr<ChatMessage> findChatMessage (const std::string &messageId) const override;
 	std::shared_ptr<ChatMessage> findChatMessage (
