59031f712fa5e1593f0865e816472cf872e0e562
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2019-11-20 12:09:48 +0100

    Added method to remove friend from rc file

--- coreapi/private_structs.h
+++ coreapi/private_structs.h
@@ -179,6 +179,7 @@ struct _LinphoneFriend{
 	LinphoneFriendList *friend_list;
 	LinphoneSubscriptionState out_sub_state;
 	int capabilities;
+	int rc_index;
 };
 
 BELLE_SIP_DECLARE_VPTR_NO_EXPORT(LinphoneFriend);
