faa5e4aec54f605252e14164b4b9c8ddde40ff29
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2020-01-17 11:06:14 +0100

    Added callback on Core when an ephemeral message has expired

--- coreapi/private_functions.h
+++ coreapi/private_functions.h
@@ -568,6 +568,7 @@ void linphone_core_notify_call_created(LinphoneCore *lc, LinphoneCall *call);
 void linphone_core_notify_version_update_check_result_received(LinphoneCore *lc, LinphoneVersionUpdateCheckResult result, const char *version, const char *url);
 void linphone_core_notify_chat_room_state_changed (LinphoneCore *lc, LinphoneChatRoom *cr, LinphoneChatRoomState state);
 void linphone_core_notify_chat_room_subject_changed (LinphoneCore *lc, LinphoneChatRoom *cr);
+void linphone_core_notify_chat_room_ephemeral_message_deleted (LinphoneCore *lc, LinphoneChatRoom *cr);
 void linphone_core_notify_qrcode_found(LinphoneCore *lc, const char *result);
 void linphone_core_notify_ec_calibration_result(LinphoneCore *lc, LinphoneEcCalibratorStatus status, int delay_ms);
 void linphone_core_notify_ec_calibration_audio_init(LinphoneCore *lc);
