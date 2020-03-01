41cbfb023c3ba77208a466b8c33fbdc6bfa202cc
Johan Pascal <johan.pascal@belledonne-communications.com>
2020-03-02 06:08:19 +0700

    Add haveLimeIk property in stream + support lime-Ik SDP attribute

--- src/c-wrapper/internal/c-sal.cpp
+++ src/c-wrapper/internal/c-sal.cpp
@@ -97,6 +97,7 @@ SalMediaDescription *sal_media_description_new(){
 		md->streams[i].rtp_port = 0;
 		md->streams[i].rtcp_port = 0;
 		md->streams[i].haveZrtpHash = 0;
+		md->streams[i].haveLimeIk = 0;
 	}
 	return md;
 }
@@ -363,6 +364,11 @@ bool_t sal_stream_description_has_dtls(const SalStreamDescription *sd) {
 
 bool_t sal_stream_description_has_zrtp(const SalStreamDescription *sd) {
 	if (sd->haveZrtpHash==1) return TRUE;
+	return FALSE;
+}
+
+bool_t sal_stream_description_has_limeIk(const SalStreamDescription *sd) {
+	if (sd->haveLimeIk==1) return TRUE;
 	return FALSE;
 }
 
