376b1cf60e1a5e221e83e415973fc48be7ad4037
Johan Pascal <johan.pascal@belledonne-communications.com>
2020-03-02 14:05:04 +0700

    Improve start ZRTP engine with audiostream condition to be friendly with non limev2 enabled peers

--- src/conference/session/audio-stream.cpp
+++ src/conference/session/audio-stream.cpp
@@ -380,12 +380,15 @@ void MS2AudioStream::render(const OfferAnswerContext &params, CallSession::State
 	if (linphone_core_media_encryption_supported(getCCore(), LinphoneMediaEncryptionZRTP) && isMain()) {
 		getMediaSessionPrivate().performMutualAuthentication();
 		LinphoneMediaEncryption requestedMediaEncryption = getMediaSessionPrivate().getParams()->getMediaEncryption();
-		//Start zrtp if remote has offered it or if local is configured for zrtp and is the offerrer. If not, defered when ACK is received
-		if ((requestedMediaEncryption == LinphoneMediaEncryptionZRTP && params.localIsOfferer)
-			|| (params.remoteStreamDescription->haveZrtpHash == 1)) {
-			startZrtpPrimaryChannel(params);
-		}else if (requestedMediaEncryption == LinphoneMediaEncryptionZRTP && !params.localIsOfferer){
-			mStartZrtpLater = true;
+		// Start ZRTP: If requested (by local config or peer giving zrtp-hash in SDP, we shall start the ZRTP engine
+		if ((requestedMediaEncryption == LinphoneMediaEncryptionZRTP) || (params.remoteStreamDescription->haveZrtpHash == 1)) {
+			// However, when we are receiver, if peer offers a lime-Ik attribute, we shall delay the start (and ZRTP Hello Packet sending)
+			// until the ACK has been received to ensure the caller got our 200 Ok (with lime-Ik in it) before starting its ZRTP engine
+			if (!params.localIsOfferer && sal_stream_description_has_limeIk(params.remoteStreamDescription)) {
+				mStartZrtpLater = true;
+			} else {
+				startZrtpPrimaryChannel(params);
+			}
 		}
 	}
 	
