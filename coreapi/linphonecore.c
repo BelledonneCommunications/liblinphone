e3cb0a63ce9f6a825da96755a113bfbbff8ef2e5
Paul Cartier <paul.cartier@belledonne-communications.com>
2020-03-18 17:01:59 +0100

    simplify _linphone_core_new_shared_with_config

--- coreapi/linphonecore.c
+++ coreapi/linphonecore.c
@@ -2656,16 +2656,11 @@ LinphoneCore *_linphone_core_new_with_config(LinphoneCoreCbs *cbs, struct _LpCon
 }
 
 LinphoneCore *_linphone_core_new_shared_with_config(LinphoneCoreCbs *cbs, struct _LpConfig *config, void *userdata, void *system_context, bool_t automatically_start, const char *app_group, bool_t main_core) {
-	LinphoneCore *core = L_INIT(Core);
-	Core::create(core);
 	linphone_config_set_string(config, "shared_core", "app_group", app_group);
+	LinphoneCore *core = _linphone_core_new_with_config(cbs, config, userdate, system_context, automatically_start);
 	core->is_main_core = main_core;
-	if (main_core) {
-		core->send_imdn_if_unregistered = false;
-	} else {
-		core->send_imdn_if_unregistered = true; // allow ios app extension to mark msg as read without being registered
-	}
-	linphone_core_init(core, cbs, config, userdata, system_context, automatically_start);
+	// allow ios app extension to mark msg as read without being registered
+	core->send_imdn_if_unregistered = !main_core;
 	return core;
 }
 
