563c46452e43ed9d5cc2ae728e9a5a516d092cb9
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-04-17 15:17:52 +0200

    Fix crash in test "Simple conference established from scratch" Do not call getOutputAudioDevice if currentCall is null

--- src/conference/session/tone-manager.cpp
+++ src/conference/session/tone-manager.cpp
@@ -406,22 +406,25 @@ void ToneManager::doStartRingbackTone(const std::shared_ptr<CallSession> &sessio
 	if (!lc->sound_conf.play_sndcard)
 		return;
 
-	MSSndCard *ringCard = nullptr;
 
-	AudioDevice * currentCallAudioDevice = getCore()->getCurrentCall()->getOutputAudioDevice();
+	MSSndCard *ringCard = lc->sound_conf.lsd_card ? lc->sound_conf.lsd_card : lc->sound_conf.play_sndcard;
 
-	// If the user changed the audio device before the ringback started, the new value will be stored in the call playback card
-	// It is NULL otherwise
-	if (currentCallAudioDevice) {
-		ringCard = currentCallAudioDevice->getSoundCard();
-	} else {
-		ringCard = lc->sound_conf.lsd_card ? lc->sound_conf.lsd_card : lc->sound_conf.play_sndcard;
+	std::shared_ptr<LinphonePrivate::Call> call = getCore()->getCurrentCall();
+	if (call) {
+		AudioDevice * audioDevice = call->getOutputAudioDevice();
+
+		// If the user changed the audio device before the ringback started, the new value will be stored in the call playback card
+		// It is NULL otherwise
+		if (audioDevice) {
+			ringCard = audioDevice->getSoundCard();
+		}
 	}
 
 	if (lc->sound_conf.remote_ring) {
 		ms_snd_card_set_stream_type(ringCard, MS_SND_CARD_STREAM_VOICE);
 		lc->ringstream = ring_start(lc->factory, lc->sound_conf.remote_ring, 2000, ringCard);
 	}
+
 }
 
 void ToneManager::doStartRingtone(const std::shared_ptr<CallSession> &session) {
