3e416673d94d13780382077108cd6a79f3126bbd
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-04-29 15:48:12 +0200

    Delete private classes of LocalConferenceEventHandler, RemoteConferenceEventHandler, Participant, ParticipantDevice, Conference, LocalConference, RemoteConference, RemoteConferenceEventHandler, LocalConferenceEventHandler Set HybridObject as base class for classes Participant, ParticipantDevice

--- src/address/identity-address.cpp
+++ src/address/identity-address.cpp
@@ -26,7 +26,6 @@
 #include "c-wrapper/c-wrapper.h"
 #include "identity-address.h"
 #include "logger/logger.h"
-#include "object/clonable-object-p.h"
 
 // =============================================================================
 
@@ -36,74 +35,59 @@ LINPHONE_BEGIN_NAMESPACE
 
 // -----------------------------------------------------------------------------
 
-class IdentityAddressPrivate : public ClonableObjectPrivate {
-public:
-	std::string scheme;
-	std::string username;
-	std::string domain;
-	std::string gruu;
-};
-
-// -----------------------------------------------------------------------------
-
-IdentityAddress::IdentityAddress (const string &address) : ClonableObject(*new IdentityAddressPrivate) {
-	L_D();
+IdentityAddress::IdentityAddress (const string &address) {
 	shared_ptr<IdentityAddress> parsedAddress = IdentityAddressParser::getInstance()->parseAddress(address);
 	if (parsedAddress != nullptr) {
 		char *tmp;
-		d->scheme = parsedAddress->getScheme();
+		scheme = parsedAddress->getScheme();
 		tmp = belle_sip_to_unescaped_string(parsedAddress->getUsername().c_str());
-		d->username = tmp;
+		username = tmp;
 		ms_free(tmp);
-		d->domain = parsedAddress->getDomain();
-		d->gruu = parsedAddress->getGruu();
+		domain = parsedAddress->getDomain();
+		gruu = parsedAddress->getGruu();
 	} else {
 		Address tmpAddress(address);
 		if (tmpAddress.isValid() && ((tmpAddress.getScheme() == "sip") || (tmpAddress.getScheme() == "sips"))) {
-			d->scheme = tmpAddress.getScheme();
-			d->username = tmpAddress.getUsername();
-			d->domain = tmpAddress.getDomain();
-			d->gruu = tmpAddress.getUriParamValue("gr");
+			scheme = tmpAddress.getScheme();
+			username = tmpAddress.getUsername();
+			domain = tmpAddress.getDomain();
+			gruu = tmpAddress.getUriParamValue("gr");
 		}
 	}
 }
 
-IdentityAddress::IdentityAddress (const Address &address) : ClonableObject(*new IdentityAddressPrivate) {
-	L_D();
-	d->scheme = address.getScheme();
-	d->username = address.getUsername();
-	d->domain = address.getDomain();
+IdentityAddress::IdentityAddress (const Address &address) {
+	scheme = address.getScheme();
+	username = address.getUsername();
+	domain = address.getDomain();
 	if (address.hasUriParam("gr"))
-		d->gruu = address.getUriParamValue("gr");
+		gruu = address.getUriParamValue("gr");
 }
 
-IdentityAddress::IdentityAddress (const IdentityAddress &other) : ClonableObject(*new IdentityAddressPrivate) {
-	L_D();
-	d->scheme = other.getScheme();
-	d->username = other.getUsername();
-	d->domain = other.getDomain();
-	d->gruu = other.getGruu();
+IdentityAddress::IdentityAddress (const IdentityAddress &other) {
+	scheme = other.getScheme();
+	username = other.getUsername();
+	domain = other.getDomain();
+	gruu = other.getGruu();
 }
 
-IdentityAddress::IdentityAddress () : ClonableObject(*new IdentityAddressPrivate) {
+IdentityAddress::IdentityAddress () {
 	
 }
 
 IdentityAddress &IdentityAddress::operator= (const IdentityAddress &other) {
-	L_D();
 	if (this != &other) {
-		d->scheme = other.getScheme();
-		d->username = other.getUsername();
-		d->domain = other.getDomain();
-		d->gruu = other.getGruu();
+		scheme = other.getScheme();
+		username = other.getUsername();
+		domain = other.getDomain();
+		gruu = other.getGruu();
 	}
 	return *this;
 }
 
 bool IdentityAddress::operator== (const IdentityAddress &other) const {
-	L_D();
 	/* Scheme is not used for comparison. sip:toto@sip.linphone.org and sips:toto@sip.linphone.org refer to the same person. */
-	return d->username == other.getUsername() && d->domain == other.getDomain() && d->gruu == other.getGruu();
+	return username == other.getUsername() && domain == other.getDomain() && gruu == other.getGruu();
 }
 
 bool IdentityAddress::operator!= (const IdentityAddress &other) const {
@@ -111,66 +95,54 @@ bool IdentityAddress::operator!= (const IdentityAddress &other) const {
 }
 
 bool IdentityAddress::operator< (const IdentityAddress &other) const {
-	L_D();
-	
-	int diff = d->username.compare(other.getUsername());
+	int diff = username.compare(other.getUsername());
 	if (diff == 0){
-		diff = d->domain.compare(other.getDomain());
+		diff = domain.compare(other.getDomain());
 		if (diff == 0){
-			diff = d->gruu.compare(other.getGruu());
+			diff = gruu.compare(other.getGruu());
 		}
 	}
 	return diff < 0;
 }
 
 bool IdentityAddress::isValid () const {
-	L_D();
-	return !d->scheme.empty() && !d->domain.empty();
+	return !scheme.empty() && !domain.empty();
 }
 
 const string &IdentityAddress::getScheme () const {
-	L_D();
-	return d->scheme;
+	return scheme;
 }
 
 void IdentityAddress::setScheme (const string &scheme) {
-	L_D();
-	d->scheme = scheme;
+	this->scheme = scheme;
 }
 
 const string &IdentityAddress::getUsername () const {
-	L_D();
-	return d->username;
+	return username;
 }
 
 void IdentityAddress::setUsername (const string &username) {
-	L_D();
-	d->username = username;
+	this->username = username;
 }
 
 const string &IdentityAddress::getDomain () const {
-	L_D();
-	return d->domain;
+	return domain;
 }
 
 void IdentityAddress::setDomain (const string &domain) {
-	L_D();
-	d->domain = domain;
+	this->domain = domain;
 }
 
 bool IdentityAddress::hasGruu () const {
-	L_D();
-	return !d->gruu.empty();
+	return !gruu.empty();
 }
 
 const string &IdentityAddress::getGruu () const {
-	L_D();
-	return d->gruu;
+	return gruu;
 }
 
 void IdentityAddress::setGruu (const string &gruu) {
-	L_D();
-	d->gruu = gruu;
+	this->gruu = gruu;
 }
 
 IdentityAddress IdentityAddress::getAddressWithoutGruu () const {
@@ -180,23 +152,22 @@ IdentityAddress IdentityAddress::getAddressWithoutGruu () const {
 }
 
 string IdentityAddress::asString () const {
-	L_D();
 	ostringstream res;
-	res << d->scheme << ":";
-	if (!d->username.empty()){
-		char *tmp = belle_sip_uri_to_escaped_username(d->username.c_str());
+	res << scheme << ":";
+	if (!username.empty()){
+		char *tmp = belle_sip_uri_to_escaped_username(username.c_str());
 		res << tmp << "@";
 		ms_free(tmp);
 	}
 	
-	if (d->domain.find(":") != string::npos) {
-		res << "[" << d->domain << "]";
+	if (domain.find(":") != string::npos) {
+		res << "[" << domain << "]";
 	} else {
-		res << d->domain;
+		res << domain;
 	}
 	
-	if (!d->gruu.empty()){
-		res << ";gr=" << d->gruu;
+	if (!gruu.empty()){
+		res << ";gr=" << gruu;
 	}
 	return res.str();
 }
