77a9e84077bc1fc009ebcc68a6239c52df89f074
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-04-29 15:48:12 +0200

    Delete private classes of LocalConferenceEventHandler, RemoteConferenceEventHandler, Participant, ParticipantDevice, Conference, LocalConference, RemoteConference, RemoteConferenceEventHandler, LocalConferenceEventHandler Set HybridObject as base class for classes Participant, ParticipantDevice

--- coreapi/factory.c
+++ coreapi/factory.c
@@ -21,7 +21,7 @@
 
 #include "c-wrapper/c-wrapper.h"
 
-#include "address/address-p.h"
+#include "address/address.h"
 #include "core/paths/paths.h"
 
 // TODO: From coreapi. Remove me later.
@@ -148,7 +148,7 @@ LinphoneFactory *linphone_factory_get(void) {
 }
 
 void linphone_factory_clean(void){
-	LinphonePrivate::AddressPrivate::clearSipAddressesCache();
+	LinphonePrivate::Address::clearSipAddressesCache();
 	if (_factory){
 		belle_sip_object_unref(_factory);
 		_factory = NULL;
