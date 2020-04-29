69410a2ce8e5ccbccd2b273884c5c4faf9feeed1
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-04-29 15:48:12 +0200

    Delete private classes of LocalConferenceEventHandler, RemoteConferenceEventHandler, Participant, ParticipantDevice, Conference, LocalConference, RemoteConference, RemoteConferenceEventHandler, LocalConferenceEventHandler Set HybridObject as base class for classes Participant, ParticipantDevice

--- src/core/core.h
+++ src/core/core.h
@@ -61,7 +61,7 @@ class LINPHONE_PUBLIC Core : public Object {
 	friend class ClientGroupChatRoomPrivate;
 	friend class ClientGroupToBasicChatRoomPrivate;
 	friend class Imdn;
-	friend class LocalConferenceEventHandlerPrivate;
+	friend class LocalConferenceEventHandler;
 	friend class MainDb;
 	friend class MainDbEventKey;
 	friend class MediaSessionPrivate;
