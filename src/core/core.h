6303c14aead8bbbaea33d6ec80dba16b225a0f16
Danmei Chen <danmei.chen@belledonne-communications.com>
2020-05-07 09:32:51 +0200

    rename callkit api

--- src/core/core.h
+++ src/core/core.h
@@ -106,8 +106,8 @@ public:
 	std::shared_ptr<Call> getCurrentCall () const;
 	LinphoneStatus pauseAllCalls ();
 	void soundcardHintCheck ();
-	void soundcardAudioSessionActivated (bool active);
-	void soundcardCallkitEnabled (bool enabled);
+	void soundcardActivateAudioSession (bool active);
+	void soundcardEnableCallkit (bool enabled);
 	LinphoneStatus terminateAllCalls ();
 
 	// ---------------------------------------------------------------------------
