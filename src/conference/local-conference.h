bb7eae80434ba3e0fe94ecc96f7f56740ad3d7d5
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-05-28 16:15:14 +0200

    Move notify to base conference class

--- src/conference/local-conference.h
+++ src/conference/local-conference.h
@@ -45,15 +45,6 @@ public:
 	bool addParticipant (const IdentityAddress &addr, const CallSessionParams *params, bool hasMedia) override;
 	bool removeParticipant (const std::shared_ptr<Participant> &participant) override;
 
-	std::shared_ptr<ConferenceParticipantEvent> notifyParticipantAdded (const Address &addr);
-	std::shared_ptr<ConferenceParticipantEvent> notifyParticipantRemoved (const Address &addr);
-	std::shared_ptr<ConferenceParticipantEvent> notifyParticipantSetAdmin (const Address &addr, bool isAdmin);
-	std::shared_ptr<ConferenceSubjectEvent> notifySubjectChanged ();
-	std::shared_ptr<ConferenceParticipantDeviceEvent> notifyParticipantDeviceAdded (const Address &addr, const Address &gruu);
-	std::shared_ptr<ConferenceParticipantDeviceEvent> notifyParticipantDeviceRemoved (const Address &addr, const Address &gruu);
-
-//	void notifyFullState (const std::string &notify, const std::shared_ptr<ParticipantDevice> &device);
-
 	void subscribeReceived (LinphoneEvent *event);
 
 protected:
