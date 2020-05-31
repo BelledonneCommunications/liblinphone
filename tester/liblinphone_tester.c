c764f2eaaf79edd77776d4073ec1f69729796e58
Johan Pascal <johan.pascal@belledonne-communications.com>
2020-05-31 12:45:55 +0200

    Add tls auth to lime client - tls certificate shall already be loaded in core when the lime client needs them, no call to the authentication requested client

--- tester/liblinphone_tester.c
+++ tester/liblinphone_tester.c
@@ -325,6 +325,7 @@ void liblinphone_tester_add_suites() {
 	bc_tester_add_suite(&group_chat_test_suite);
 	bc_tester_add_suite(&secure_group_chat_test_suite);
 	bc_tester_add_suite(&ephemeral_group_chat_test_suite);
+	bc_tester_add_suite(&lime_server_auth_test_suite);
 #endif
 	bc_tester_add_suite(&tunnel_test_suite);
 	bc_tester_add_suite(&offeranswer_test_suite);
