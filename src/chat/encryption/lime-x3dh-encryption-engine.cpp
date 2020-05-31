f3a7f245633cf92ee3db8a7c20be989a0298e2a9
Johan Pascal <johan.pascal@belledonne-communications.com>
2020-05-31 12:45:55 +0200

    Add tls auth to lime client - tls certificate shall already be loaded in core when the lime client needs them, no call to the authentication requested client

--- src/chat/encryption/lime-x3dh-encryption-engine.cpp
+++ src/chat/encryption/lime-x3dh-encryption-engine.cpp
@@ -71,14 +71,87 @@ void LimeManager::processResponse (void *data, const belle_http_response_event_t
 void LimeManager::processAuthRequested (void *data, belle_sip_auth_event_t *event) noexcept {
 	X3dhServerPostContext *userData = static_cast<X3dhServerPostContext *>(data);
 	shared_ptr<Core> core = userData->core;
+	/* Notes: when registering on the Lime server, the user is already registered on the flexisip server
+	 * the requested auth info shall thus be present in linphone core (except if registering methods are differents on flexisip and lime server - very unlikely)
+	 * This request will thus not use the auth requested callback to get the information
+	 * - Stored auth information in linphone core are indexed by username/domain */
 
-	const char *realm = belle_sip_auth_event_get_realm(event);
-	const char *username = belle_sip_auth_event_get_username(event);
-	const char *domain = belle_sip_auth_event_get_domain(event);
-	const char *algorithm = belle_sip_auth_event_get_algorithm(event);
+	switch (belle_sip_auth_event_get_mode(event)) {
+		case BELLE_SIP_AUTH_MODE_HTTP_DIGEST:{
+			const char *realm = belle_sip_auth_event_get_realm(event);
+			const char *username = belle_sip_auth_event_get_username(event);
+			const char *domain = belle_sip_auth_event_get_domain(event);
+			const char *algorithm = belle_sip_auth_event_get_algorithm(event);
 
-	const LinphoneAuthInfo *auth_info = _linphone_core_find_auth_info(core->getCCore(), realm, username, domain, algorithm, TRUE);
-	linphone_auth_info_fill_belle_sip_event(auth_info, event);
+			const LinphoneAuthInfo *auth_info = _linphone_core_find_auth_info(core->getCCore(), realm, username, domain, algorithm, TRUE);
+			linphone_auth_info_fill_belle_sip_event(auth_info, event);
+		}
+		break;
+		case BELLE_SIP_AUTH_MODE_TLS: {
+			/* extract username and domain from the GRUU stored in userData->username */
+			auto address = IdentityAddress(userData->username);
+			const char *cert_chain_path = nullptr;
+			const char *key_path = nullptr;
+			const char *cert_chain = nullptr;
+			const char *key = nullptr;
+			const LinphoneAuthInfo *auth_info = _linphone_core_find_indexed_tls_auth_info(core->getCCore(), address.getUsername().data(), address.getDomain().data());
+			if (auth_info) { /* tls_auth_info found something */
+				if (linphone_auth_info_get_tls_cert(auth_info) && linphone_auth_info_get_tls_key(auth_info)) {
+					cert_chain = linphone_auth_info_get_tls_cert(auth_info);
+					key = linphone_auth_info_get_tls_key(auth_info);
+				} else if (linphone_auth_info_get_tls_cert_path(auth_info) && linphone_auth_info_get_tls_key_path(auth_info)) {
+					cert_chain_path = linphone_auth_info_get_tls_cert_path(auth_info);
+					key_path = linphone_auth_info_get_tls_key_path(auth_info);
+				}
+			} else { /* get directly from linphonecore
+				    or given in the sip/client_cert_chain in the config file, no username/domain associated with it, last resort try
+				   it shall work if we have only one user */
+				cert_chain = linphone_core_get_tls_cert(core->getCCore());
+				key = linphone_core_get_tls_key(core->getCCore());
+				if (!cert_chain || !key) {
+					cert_chain_path = linphone_core_get_tls_cert_path(core->getCCore());
+					key_path = linphone_core_get_tls_key_path(core->getCCore());
+				}
+			}
+
+			if (cert_chain != nullptr && key != nullptr) {
+				belle_sip_certificates_chain_t *bs_cert_chain = belle_sip_certificates_chain_parse(cert_chain, strlen(cert_chain), BELLE_SIP_CERTIFICATE_RAW_FORMAT_PEM);
+				belle_sip_signing_key_t *bs_key = belle_sip_signing_key_parse(key, strlen(key), nullptr);
+				if (bs_cert_chain && bs_key) {
+					belle_sip_object_ref((belle_sip_object_t *)bs_cert_chain);
+					belle_sip_object_ref((belle_sip_object_t *)bs_key);
+					belle_sip_auth_event_set_signing_key(event,  bs_key);
+					belle_sip_auth_event_set_client_certificates_chain(event, bs_cert_chain);
+					belle_sip_object_unref((belle_sip_object_t *)bs_cert_chain);
+					belle_sip_object_unref((belle_sip_object_t *)bs_key);
+				}
+			} else if (cert_chain_path != nullptr && key_path != nullptr) {
+				belle_sip_certificates_chain_t *bs_cert_chain = belle_sip_certificates_chain_parse_file(cert_chain_path, BELLE_SIP_CERTIFICATE_RAW_FORMAT_PEM);
+				belle_sip_signing_key_t *bs_key = belle_sip_signing_key_parse_file(key_path, nullptr);
+				if (bs_cert_chain && bs_key) {
+					belle_sip_object_ref((belle_sip_object_t *)bs_cert_chain);
+					belle_sip_object_ref((belle_sip_object_t *)bs_key);
+					belle_sip_auth_event_set_signing_key(event,  bs_key);
+					belle_sip_auth_event_set_client_certificates_chain(event, bs_cert_chain);
+					belle_sip_object_unref((belle_sip_object_t *)bs_cert_chain);
+					belle_sip_object_unref((belle_sip_object_t *)bs_key);
+				}
+			} else {
+				lInfo() << "Lime encryption engine could not retrieve any client certificate upon server's request";
+				// To enable callback:
+				//  - create an AuthInfo object with username and domain
+				//  - call linphone_core_notify_authentication_requested on it to give the app a chance to fill the auth_info.
+				//  - call again _linphone_core_find_indexed_tls_auth_info to retrieve the auth_info set by the callback.
+				//  Not done as we assume that authentication on flexisip server was performed before
+				//  so the application layer already got a chance to set the correct auth_info in the core
+				return;
+			}
+		}
+		break;
+		default:
+			lError() << "Lime X3DH server connection gets an auth event of unexpected type";
+		break;
+	}
 }
 
 LimeManager::LimeManager (
@@ -916,10 +989,12 @@ void LimeX3dhEncryptionEngine::onRegistrationStateChanged (
 	try {
 		if (!limeManager->is_user(localDeviceId)) {
 			// create user if not exist
-			//lime::limeCallback callback = setLimeCallback("creating user " + localDeviceId);
-			//limeManager->create_user(localDeviceId, x3dhServerUrl, curve, callback);
 			limeManager->create_user(localDeviceId, x3dhServerUrl, curve, [lc, localDeviceId](lime::CallbackReturn returnCode, string info) {
-						lInfo() << "[LIME] user "<< localDeviceId <<" creation successful";
+						if (returnCode==lime::CallbackReturn::success) {
+							lInfo() << "[LIME] user "<< localDeviceId <<" creation successful";
+						} else {
+							lWarning() << "[LIME] user "<< localDeviceId <<" creation failed";
+						}
 						linphone_core_notify_imee_user_registration(lc, returnCode==lime::CallbackReturn::success, localDeviceId.data(), info.data());
 					});
 			lastLimeUpdate = ms_time(NULL);
