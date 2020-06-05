efb607f5660d273b1670ba8d6b548fc95fb06f29
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-06-05 13:25:53 +0200

    Make src/conference a base class of conference.cc

--- coreapi/linphonecore.c
+++ coreapi/linphonecore.c
@@ -7733,16 +7733,19 @@ LinphoneConference *linphone_core_create_conference_with_params(LinphoneCore *lc
 		LinphoneConferenceParams *params2 = linphone_conference_params_clone(params);
 		
 		conf_method_name = lp_config_get_string(lc->config, "misc", "conference_type", "local");
+		LinphoneAddress *identity = linphone_address_new(linphone_core_get_identity(lc));
 		if (strcasecmp(conf_method_name, "local") == 0) {
-			conf = linphone_local_conference_new_with_params(lc, params2);
+			conf = linphone_local_conference_new_with_params(lc, identity, params2);
 		} else if (!serverMode && strcasecmp(conf_method_name, "remote") == 0) {
-			conf = linphone_remote_conference_new_with_params(lc, params2);
+			conf = linphone_remote_conference_new_with_params(lc, identity, params2);
 		} else {
 			ms_error("'%s' is not a valid conference method", conf_method_name);
 			linphone_conference_params_unref(params2);
+			linphone_address_unref(identity);
 			return NULL;
 		}
 		linphone_conference_params_unref(params2);
+		linphone_address_unref(identity);
 		if (!serverMode) {
 			linphone_core_set_conference(lc, conf);
 			linphone_conference_set_state_changed_callback(conf, _linphone_core_conference_state_changed, lc);
