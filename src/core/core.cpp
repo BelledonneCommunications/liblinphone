904a26445c439f24e1399dfaf22871f90c040a64
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-06-12 16:27:27 +0200

    Fix crash in test "Eject from 3 participants conference" due to double free of audioVideoConference unordered map

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -189,6 +189,10 @@ void CorePrivate::uninit() {
 
 	chatRoomsById.clear();
 
+	for (const auto &audioVideoConference : q->audioVideoConferenceById) {
+		// Terminate audio video conferences just before core is stopped
+		audioVideoConference.second->terminate();
+	}
 	q->audioVideoConferenceById.clear();
 
 	noCreatedClientGroupChatRooms.clear();
@@ -1017,6 +1021,7 @@ void Core::deleteAudioVideoConference(const shared_ptr<const MediaConference::Co
 	if (it != audioVideoConferenceById.cend()) {
 		audioVideoConferenceById.erase(it);
 	}
+
 }
 
 LINPHONE_END_NAMESPACE
