6278c2dce6bd5eda64d9724534b3fcb0c40dd84c
AndreaGianarda <andrea.gianarda@belledonne-communications.com>
2020-06-15 07:01:15 +0200

    Fix compilation under Windows: - Add LINPHONE_PUBLIC to COnference, LocalConference and   RemoteConference for audio video conferences - Rename liphone_conference_add_participant as   linphone_conference_add_participant_with_call in conference_private to   avoid clash with definition in conference.h

--- coreapi/linphonecore.c
+++ coreapi/linphonecore.c
@@ -7782,7 +7782,7 @@ LinphoneStatus linphone_core_add_to_conference(LinphoneCore *lc, LinphoneCall *c
 		linphone_conference_params_unref(params);
 		linphone_conference_unref(conference); /*actually linphone_core_create_conference_with_params() takes a ref for lc->conf_ctx */
 	}
-	if(conference) return linphone_conference_add_participant(lc->conf_ctx, call);
+	if(conference) return linphone_conference_add_participant_with_call(lc->conf_ctx, call);
 	else return -1;
 }
 
