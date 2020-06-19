802fbdf36437b1fc68f96658d8daf20c5a7ea3e1
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-06-19 13:26:43 +0200

    Delete unnecessary debug prints

--- src/call/call.cpp
+++ src/call/call.cpp
@@ -376,7 +376,7 @@ void CallPrivate::onCallSessionStateChanged (const shared_ptr<CallSession> &sess
 			if (remoteContactAddress.hasParam("isfocus")) {
 				ConferenceId remoteConferenceId = ConferenceId(remoteContactAddress, q->getLocalAddress());
 				// It is expected that the core of the remote conference is the participant one
-				remoteConf = std::shared_ptr<MediaConference::RemoteConference>(new MediaConference::RemoteConference(q->getCore(), remoteContactAddress, remoteConferenceId, nullptr, ConferenceParams::create(q->getCore()->getCCore())), [](MediaConference::RemoteConference * c){printf("%p - destroying remote conference\n", c); c->unref();});
+				remoteConf = std::shared_ptr<MediaConference::RemoteConference>(new MediaConference::RemoteConference(q->getCore(), remoteContactAddress, remoteConferenceId, nullptr, ConferenceParams::create(q->getCore()->getCCore())), [](MediaConference::RemoteConference * c){c->unref();});
 
 				#ifdef HAVE_ADVANCED_IM
 				remoteConf->eventHandler->subscribe(remoteConferenceId);
