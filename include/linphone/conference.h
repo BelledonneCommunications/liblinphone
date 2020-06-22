49b8e1dc9340e3de02a8523d1428df26b14db319
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-06-22 13:44:43 +0200

    Delete linphone_conference_remove_participant_with_call and linphone_conference_add_participant

--- include/linphone/conference.h
+++ include/linphone/conference.h
@@ -163,7 +163,7 @@ LINPHONE_PUBLIC LinphoneStatus linphone_conference_invite_participants(LinphoneC
  * @param conf The #LinphoneConference object. @notnil
  * @param call a #LinphoneCall that has to be added to the conference. @notnil
  */
-LINPHONE_PUBLIC LinphoneStatus linphone_conference_add_participant(LinphoneConference *obj, LinphoneCall *call);
+LINPHONE_PUBLIC LinphoneStatus linphone_conference_add_participant_with_call(LinphoneConference *obj, LinphoneCall *call);
 
 /**
  * Join a participant to the conference.
@@ -251,7 +251,6 @@ LinphoneConference *linphone_remote_conference_new(LinphoneCore *core, LinphoneA
 LinphoneConference *linphone_remote_conference_new_with_params(LinphoneCore *core, LinphoneAddress * addr, const LinphoneConferenceParams *params);
 
 int linphone_conference_add_participant_with_call(LinphoneConference *obj, LinphoneCall *call);
-int linphone_conference_remove_participant_with_call(LinphoneConference *obj, LinphoneCall *call);
 int linphone_conference_get_size(const LinphoneConference *obj);
 
 /* This is actually only used by the ToneManager. TODO: encapsulate this better. */
