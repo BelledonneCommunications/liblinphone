749c8e194bb7e7c9c827163d986969fc0468d28f
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-06-25 14:43:12 +0200

    Inherit confernece in client and server group chat room as it is required when calling virtual methods from the base class

--- src/chat/chat-room/basic-chat-room.cpp
+++ src/chat/chat-room/basic-chat-room.cpp
@@ -99,22 +99,22 @@ bool BasicChatRoom::addParticipant (
 bool BasicChatRoom::addParticipants (
 	const list<IdentityAddress> &addresses
 ) {
-	lError() << "addParticipants() is not allowed on a BasicChatRoom";
+	lError() << __func__ << " is not allowed on a BasicChatRoom";
 	return false;
 }
 
 bool BasicChatRoom::removeParticipant (const shared_ptr<Participant> &) {
-	lError() << "removeParticipant() is not allowed on a BasicChatRoom";
+	lError() << __func__ << " is not allowed on a BasicChatRoom";
 	return false;
 }
 
 bool BasicChatRoom::removeParticipants (const list<shared_ptr<Participant>> &) {
-	lError() << "removeParticipants() is not allowed on a BasicChatRoom";
+	lError() << __func__ << " is not allowed on a BasicChatRoom";
 	return false;
 }
 
 shared_ptr<Participant> BasicChatRoom::findParticipant (const IdentityAddress &) const {
-	lError() << "findParticipant() is not allowed on a BasicChatRoom";
+	lError() << __func__ << " is not allowed on a BasicChatRoom";
 	return nullptr;
 }
 
@@ -133,7 +133,7 @@ const list<shared_ptr<Participant>> &BasicChatRoom::getParticipants () const {
 }
 
 void BasicChatRoom::setParticipantAdminStatus (const shared_ptr<Participant> &, bool) {
-	lError() << "setParticipantAdminStatus() is not allowed on a BasicChatRoom";
+	lError() << __func__ << " is not allowed on a BasicChatRoom";
 }
 
 const string &BasicChatRoom::getSubject () const {
@@ -147,15 +147,15 @@ void BasicChatRoom::setSubject (const string &subject) {
 }
 
 void BasicChatRoom::join () {
-	lError() << "join() is not allowed on a BasicChatRoom";
+	lError() << __func__ << " is not allowed on a BasicChatRoom";
 }
 
 void BasicChatRoom::join (const IdentityAddress &participantAddress) {
-	lError() << "join() is not allowed on a BasicChatRoom";
+	lError() << __func__ << " is not allowed on a BasicChatRoom";
 }
 
 void BasicChatRoom::leave () {
-	lError() << "leave() is not allowed on a BasicChatRoom";
+	lError() << __func__ << " is not allowed on a BasicChatRoom";
 }
 
 const ConferenceId &BasicChatRoom::getConferenceId () const {
@@ -177,5 +177,9 @@ void BasicChatRoom::setState (ConferenceInterface::State newState) {
 
 ConferenceInterface::State BasicChatRoom::getState () const {
 	return state;
+}
+
+void BasicChatRoom::addListener(std::shared_ptr<ConferenceListenerInterface> listener) {
+	lError() << __func__ << " is not allowed on a BasicChatRoom";
 }
 LINPHONE_END_NAMESPACE
