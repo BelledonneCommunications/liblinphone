e176c1254b6c179ee6bb4af6476f2f2e2b4b4584
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-07-10 14:27:55 +0200

    Fix compile errors after rebase

--- coreapi/linphonecore.c
+++ coreapi/linphonecore.c
@@ -7771,7 +7771,6 @@ LinphoneConference *linphone_core_create_conference_with_params(LinphoneCore *lc
 	/* In server mode, it is allowed to create multiple conferences. */
 	if (lc->conf_ctx == NULL || serverMode) {
 		LinphoneConferenceParams *params2 = linphone_conference_params_clone(params);
-
 		conf_method_name = linphone_config_get_string(lc->config, "misc", "conference_type", "local");
 		LinphoneAddress *identity = linphone_address_new(linphone_core_get_identity(lc));
 		if (strcasecmp(conf_method_name, "local") == 0) {
