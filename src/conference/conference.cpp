0c08dda25de5ae8cf44f381829c152744f374523
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-07-20 17:25:00 +0200

    Comment out tests that crash set isFocus of me to TRUE

--- src/conference/conference.cpp
+++ src/conference/conference.cpp
@@ -45,6 +45,7 @@ Conference::Conference (
 	const std::shared_ptr<ConferenceParams> params
 ) : CoreAccessor(core) {
 	this->me = Participant::create(this,myAddress);
+	this->me->setFocus(true);
 	this->listener = listener;
 	this->update(*params);
 	this->confParams->setMe(myAddress);
