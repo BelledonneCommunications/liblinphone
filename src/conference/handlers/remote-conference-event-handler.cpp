b62f6f46ae4bde0a9fde158581d751ef1fff10e9
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-07-23 17:15:04 +0200

    Unref subscribe within invalidate

--- src/conference/handlers/remote-conference-event-handler.cpp
+++ src/conference/handlers/remote-conference-event-handler.cpp
@@ -302,7 +302,6 @@ void RemoteConferenceEventHandler::subscribe () {
 void RemoteConferenceEventHandler::unsubscribePrivate () {
 	if (lev) {
 		linphone_event_terminate(lev);
-//		linphone_event_unref(lev);
 		lev = nullptr;
 	}
 }
@@ -326,6 +325,7 @@ void RemoteConferenceEventHandler::onEnteringForeground () {
 }
 
 void RemoteConferenceEventHandler::invalidateSubscription () {
+	linphone_event_unref(lev);
 	lev = nullptr;
 }
 
