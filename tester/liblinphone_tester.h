1ea40f7f37fdc795b207375d0abd4137150733a2
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-07-28 12:44:53 +0200

    Take out of confernece paused calls

--- tester/liblinphone_tester.h
+++ tester/liblinphone_tester.h
@@ -562,7 +562,7 @@ void destroy_mgr_in_conference(LinphoneCoreManager *mgr);
 LinphoneStatus add_participant_to_local_conference_through_invite(bctbx_list_t *lcs, LinphoneCoreManager * conf_mgr, bctbx_list_t *participants, const LinphoneCallParams *params);
 LinphoneStatus accept_call_in_local_conference(bctbx_list_t *lcs, LinphoneCoreManager * conf_mgr, LinphoneCoreManager * participant_mgr);
 LinphoneStatus add_calls_to_local_conference(bctbx_list_t *lcs, LinphoneCoreManager * conf_mgr, bctbx_list_t *new_participants);
-LinphoneStatus remove_participant_from_local_conference(bctbx_list_t *lcs, LinphoneCoreManager * conf_mgr, LinphoneCoreManager * participant_mgr);
+LinphoneStatus remove_participant_from_local_conference(bctbx_list_t *lcs, LinphoneCoreManager * conf_mgr, LinphoneCoreManager * participant_mgr, bool_t through_call_pause);
 LinphoneStatus terminate_local_conference(bctbx_list_t *lcs, LinphoneCoreManager * conf_mgr);
 void finish_terminate_local_conference(bctbx_list_t *lcs);
 LinphoneConferenceServer* linphone_conference_server_new(const char *rc_file, bool_t do_registration);
