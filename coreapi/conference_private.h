f0bc3f992758b77682bf4ace821b9b33a91be103
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-07-30 09:25:48 +0200

    Try to put call in pause or update its call params if the conference is not in termination pending state

--- coreapi/conference_private.h
+++ coreapi/conference_private.h
@@ -107,10 +107,14 @@ public:
 
 	// Addressing compilation error -Werror=overloaded-virtual
 	using LinphonePrivate::Conference::removeParticipant;
-	virtual int removeParticipant(std::shared_ptr<LinphonePrivate::Call> call) = 0;
 	virtual int removeParticipantDevice(std::shared_ptr<LinphonePrivate::Call> call);
+	virtual int removeParticipant(std::shared_ptr<LinphonePrivate::Call> call) = 0;
 	virtual int removeParticipant(const IdentityAddress &addr) = 0;
 	virtual bool removeParticipant(const std::shared_ptr<LinphonePrivate::Participant> &participant) override;
+
+//	virtual int removeParticipantFromConference(std::shared_ptr<LinphonePrivate::Call> call) = 0;
+//	virtual int removeParticipantFromConference(const IdentityAddress &addr) = 0;
+//	virtual bool removeParticipantFromConference(const std::shared_ptr<LinphonePrivate::Participant> &participant) override;
 	virtual bool removeParticipants (const std::list<std::shared_ptr<LinphonePrivate::Participant>> &participants) override;
 
 	virtual int terminate() = 0;
