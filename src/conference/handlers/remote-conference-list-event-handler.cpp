b8c932a86f417e4764581bec11f300c84acfe3f0
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-08-03 13:38:28 +0200

    Fix chat room tests

--- src/conference/handlers/remote-conference-list-event-handler.cpp
+++ src/conference/handlers/remote-conference-list-event-handler.cpp
@@ -110,6 +110,8 @@ void RemoteConferenceListEventHandler::subscribe () {
 	LinphoneAddress *rlsAddr = linphone_address_new(factoryUri);
 
 	lev = linphone_core_create_subscribe(lc, rlsAddr, "conference", 600);
+	// FIXME: Take a ref of the event because when linphone_event_release is called upon termination of the subscription. This function unrefs the event causing a dangling pointer to be left to the event handler
+//	linphone_event_ref(lev);
 	char *from = linphone_address_as_string(linphone_proxy_config_get_contact(linphone_core_get_default_proxy_config(getCore()->getCCore())));
 	lev->op->setFrom(from);
 	bctbx_free(from);
@@ -135,6 +137,7 @@ void RemoteConferenceListEventHandler::unsubscribe () {
 }
 
 void RemoteConferenceListEventHandler::invalidateSubscription () {
+//	linphone_event_unref(lev);
 	lev = nullptr;
 }
 
