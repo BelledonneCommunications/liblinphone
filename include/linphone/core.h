94755c15b8cf244125d0771cc7a2fe3b3181fe39
QuentinArguillere <QuentinA@MacBook-Pro.local>
2020-08-18 11:42:01 +0200

    Add call to new method audio_stream_enable_mic, and flag disable_record_on_mute to the core configuration

--- include/linphone/core.h
+++ include/linphone/core.h
@@ -2429,6 +2429,28 @@ LINPHONE_PUBLIC void linphone_core_set_mic_gain_db(LinphoneCore *core, float lev
 LINPHONE_PUBLIC float linphone_core_get_mic_gain_db(LinphoneCore *core);
 
 /**
+ * Calling this method with disable=true will cause the microhone to be completely deactivated when muted,
+ * when given possible by the implementation on the platform on which liblinphone is running.
+ * Otherwise, sound recording remains active but silence is sent instead of recorded audio.
+ * Playback sound will be briefly interrupted while the audio is reconfigured.
+ * Currently only implemented for IOS, it will also disable Apple's microphone recording indicator when microphone is muted.
+ *
+ * @param core #LinphoneCore object @notnil
+ * @param disable True if you wish to entirely stop the audio recording when muting the microphone.
+ * @ingroup media_parameters
+ */
+LINPHONE_PUBLIC void linphone_core_set_disable_record_on_mute(LinphoneCore *core, bool_t disable);
+
+/**
+ * Get whether the microphone will be completely deactivated when muted. Please refer to linphone_core_set_disable_record_on_mute().
+ *
+ * @param core #LinphoneCore object @notnil
+ * @return  True if you wish to entirely stop the audio recording when muting the microphone.
+ * @ingroup media_parameters
+ */
+LINPHONE_PUBLIC bool_t linphone_core_get_disable_record_on_mute(LinphoneCore *core);
+
+/**
  * Allow to control play level before entering sound card:  gain in db
  * @param core #LinphoneCore object @notnil
  * @param level The new play level
