1efbbc0b9835589ada409af63f1ce24acaefee87
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-09-25 16:15:21 +0200

    Fix compilation with ninja

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -388,7 +388,7 @@ bool CorePrivate::setInputAudioDevice(AudioDevice *audioDevice) {
 	}
 
 	if (static_cast<unsigned int>(q->audioVideoConferenceById.size()) > 0) {
-		for (const auto audioVideoConference : q->audioVideoConferenceById) {
+		for (const auto & audioVideoConference : q->audioVideoConferenceById) {
 			audioVideoConference.second->getAudioControlInterface()->setInputDevice(audioDevice);
 		}
 	}
@@ -412,7 +412,7 @@ bool CorePrivate::setOutputAudioDevice(AudioDevice *audioDevice) {
 	}
 
 	if (static_cast<unsigned int>(q->audioVideoConferenceById.size()) > 0) {
-		for (const auto audioVideoConference : q->audioVideoConferenceById) {
+		for (const auto & audioVideoConference : q->audioVideoConferenceById) {
 			audioVideoConference.second->getAudioControlInterface()->setOutputDevice(audioDevice);
 		}
 	}
