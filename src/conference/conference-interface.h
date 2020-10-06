26ccc97c95b69dc45ab2f8e6f51a18396fd46e65
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-10-06 15:20:22 +0200

    Finalize work on conference API: - further implement RFC4575 media events - the conference sends out the conference capabilities in its full state notify - when the conference media change, a media event will be sent out to inform participants (onAvailableMediaChanged) - when a remote participant pauses its call and conference event package is enabled, a participant device media changed event is sent (onParticipantDeviceMediaChanged) - prevent that a core that supports the event conference package pauses individual calls in a conference - rework of address comparison using belle sip functions - add conference parameter to allow conference with one participant

--- src/conference/conference-interface.h
+++ src/conference/conference-interface.h
@@ -29,7 +29,6 @@
 
 LINPHONE_BEGIN_NAMESPACE
 
-class ConferenceAvailableMediaEvent;
 class ConferenceListenerInterface;
 class IdentityAddress;
 class CallSessionParams;
@@ -255,6 +254,13 @@ public:
 	virtual void onParticipantRemoved (const std::shared_ptr<ConferenceParticipantEvent> &event, const std::shared_ptr<Participant> &participant) {}
 
 	/*
+	* This fonction is called each time a participant device changes its available media
+	* @param[in] event informations related to the device's participant.
+	* @param[in] device participant device that changed its media capabilities
+	*/
+	virtual void onParticipantDeviceMediaChanged (const std::shared_ptr<ConferenceParticipantDeviceEvent> &event, const std::shared_ptr<ParticipantDevice> &device) {};
+
+	/*
 	 * This fonction is called each time a new admin is set by the focus after full state notification.
 	 * @param[in] event informations related to the new admin participant. @notnil
 	 * @param[in] participant participant whose admin status changed. @notnil
@@ -350,17 +356,6 @@ class LINPHONE_PUBLIC ConferenceFactoryInterface {
 	*/
 	std::shared_ptr<ConferenceInterface>& createConference(const std::shared_ptr<ConferenceParamsInterface> &params,
 	const std::list<IdentityAddress> &participants);
-};
-
-/*
- * Event used to repport media availability changes from a conference.
- **/
-class ConferenceAvailableMediaEvent :  public EventLog {
-	/*
-	 * Can be audio, video, text
-	 *@return list of available media type for this conference;
-	 */
-	const std::list<std::string> &getAvailableMediaType () const;
 };
 
 std::ostream& operator<<(std::ostream& lhs, ConferenceInterface::State e);
