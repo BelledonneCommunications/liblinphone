cb536bdfb4f77cbff76ffae5dfdf2331fa0c9706
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-10-09 12:03:47 +0200

    Redesignate admin if no admin in local conference

--- coreapi/conference_private.h
+++ coreapi/conference_private.h
@@ -224,6 +224,8 @@ public:
 	virtual void setParticipantAdminStatus (const std::shared_ptr<LinphonePrivate::Participant> &participant, bool isAdmin) override;
 
 private:
+
+	void chooseAnotherAdminIfNoneInConference();
 	void addLocalEndpoint();
 	void removeLocalEndpoint();
 	std::unique_ptr<MixerSession> mMixerSession;
