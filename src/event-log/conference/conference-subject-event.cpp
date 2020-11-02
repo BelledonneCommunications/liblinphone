c56817b54ec346f09e6f4045f3eb9d14a787b1c3
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-11-02 16:54:12 +0100

    Fix compile errors

--- src/event-log/conference/conference-subject-event.cpp
+++ src/event-log/conference/conference-subject-event.cpp
@@ -17,6 +17,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <sstream>
+
 #include "conference-notified-event-p.h"
 #include "conference-subject-event.h"
 
