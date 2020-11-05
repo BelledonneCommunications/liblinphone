b7aa90e4dcd657a437b6aef9900cd434bc26a436
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-11-05 15:39:50 +0100

    Fix name of C API participant device indentity toString function

--- src/c-wrapper/api/c-participant-device-identity.cpp
+++ src/c-wrapper/api/c-participant-device-identity.cpp
@@ -74,6 +74,6 @@ void linphone_participant_device_identity_unref (LinphoneParticipantDeviceIdenti
 #endif
 }
 
-char * linphone_participant_device_to_string (const LinphoneParticipantDeviceIdentity *deviceIdentity) {
+char * linphone_participant_device_identity_to_string (const LinphoneParticipantDeviceIdentity *deviceIdentity) {
 	return ms_strdup(L_GET_CPP_PTR_FROM_C_OBJECT(deviceIdentity)->toString().c_str());
 }
