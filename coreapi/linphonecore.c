f9d0b5a4360929d932ffb7c2e45332b96398ca96
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-11-16 16:45:16 +0100

    Restart focus in local conference test

--- coreapi/linphonecore.c
+++ coreapi/linphonecore.c
@@ -6773,12 +6773,6 @@ static void _linphone_core_stop_async_start(LinphoneCore *lc) {
  * and change the state to "Off"
  */
 static void _linphone_core_stop_async_end(LinphoneCore *lc) {
-	L_GET_PRIVATE_FROM_C_OBJECT(lc)->uninit();
-
-	lc->chat_rooms = bctbx_list_free_with_data(lc->chat_rooms, (bctbx_list_free_func)linphone_chat_room_unref);
-
-	getPlatformHelpers(lc)->onLinphoneCoreStop();
-
 	/* save all config */
 	friends_config_uninit(lc);
 	sip_config_uninit(lc);
@@ -6789,6 +6783,11 @@ static void _linphone_core_stop_async_end(LinphoneCore *lc) {
 	codecs_config_uninit(lc);
 
 	sip_setup_unregister_all();
+
+	// Call uninit here because there may be the need to access DB while unregistering
+	L_GET_PRIVATE_FROM_C_OBJECT(lc)->uninit();
+	lc->chat_rooms = bctbx_list_free_with_data(lc->chat_rooms, (bctbx_list_free_func)linphone_chat_room_unref);
+	getPlatformHelpers(lc)->onLinphoneCoreStop();
 
 	if (linphone_config_needs_commit(lc->config)) linphone_config_sync(lc->config);
 
