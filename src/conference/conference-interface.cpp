ae1aa14578fa598396fb3cfa074f10e1732419be
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2020-11-17 12:17:20 +0100

    Fixed test & feature implementation

--- src/conference/conference-interface.cpp
+++ src/conference/conference-interface.cpp
@@ -36,7 +36,6 @@ std::ostream& operator<<(std::ostream& lhs, ConferenceInterface::State e) {
 		case ConferenceInterface::State::Terminated: lhs << "Terminated"; break;
 		case ConferenceInterface::State::TerminationFailed: lhs << "TerminationFailed"; break;
 		case ConferenceInterface::State::Deleted: lhs << "Deleted"; break;
-		case ConferenceInterface::State::ExhumePending: lhs << "ExhumePending"; break;
 	}
 	return lhs;
 }
