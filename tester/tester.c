13475d1df6799f5d937498c3fba62fc76e2384c9
Johan Pascal <johan.pascal@belledonne-communications.com>
2020-12-11 14:40:07 +0100

    Disable Apple DNS Service for testing

--- tester/tester.c
+++ tester/tester.c
@@ -25,6 +25,7 @@
 #include "liblinphone_tester.h"
 #include <bctoolbox/tester.h>
 #include "tester_utils.h"
+#include "belle-sip/sipstack.h"
 
 #define SKIP_PULSEAUDIO 1
 
@@ -146,6 +147,7 @@ void configure_lc(LinphoneCore *lc, const char *path, void *user_data) {
 
 	sal_enable_test_features(linphone_core_get_sal(lc), TRUE);
 
+	belle_sip_stack_set_dns_engine(sal_get_stack_impl(linphone_core_get_sal(lc)), BELLE_SIP_DNS_DNS_C); // Make sure we are not using Apple DNS Service during liblinphone tests
 	setup_dns(lc, path);
 }
 
