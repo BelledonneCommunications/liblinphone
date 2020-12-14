4f4d16dc99762801c5ca34bac1ed2f603dc21a98
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-12-14 10:54:26 +0100

    Convert char* to string using L_C_TO_STRING Alocate media descpriton using new instread of malloc

--- src/conference/session/ms2-stream.cpp
+++ src/conference/session/ms2-stream.cpp
@@ -334,10 +334,10 @@ void MS2Stream::getRtpDestination(const OfferAnswerContext &params, RtpAddressIn
 		}
 	}
 	
-	info->rtpAddr = stream->rtp_addr.empty() == false ? stream->rtp_addr.c_str() : params.resultMediaDescription->addr;
+	info->rtpAddr = stream->rtp_addr.empty() == false ? stream->rtp_addr : params.resultMediaDescription->addr;
 	bool isMulticast = !!ms_is_multicast(info->rtpAddr.c_str());
 	info->rtpPort = stream->rtp_port;
-	info->rtcpAddr = stream->rtcp_addr.empty() == false ? stream->rtcp_addr.c_str() : info->rtpAddr;
+	info->rtcpAddr = stream->rtcp_addr.empty() == false ? stream->rtcp_addr : info->rtpAddr;
 	info->rtcpPort = (linphone_core_rtcp_enabled(getCCore()) && !isMulticast) ? (stream->rtcp_port ? stream->rtcp_port : stream->rtp_port + 1) : 0;
 }
 
