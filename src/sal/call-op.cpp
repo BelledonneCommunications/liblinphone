e42992b8650775f575e36a04480c5112da47f661
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2021-01-06 12:10:30 +0100

    Delete: - src/c-wrapper/internal/c-sal-media-description.cpp - src/c-wrapper/internal/c-sal-media-description.h - src/c-wrapper/internal/c-sal-stream-description.cpp - src/c-wrapper/internal/c-sal-stream-description.h

--- src/sal/call-op.cpp
+++ src/sal/call-op.cpp
@@ -17,8 +17,6 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "c-wrapper/internal/c-sal-media-description.h"
-#include "c-wrapper/internal/c-sal-stream-description.h"
 #include "bellesip_sal/sal_impl.h"
 #include "offeranswer.h"
 #include "sal/call-op.h"
