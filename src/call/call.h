7afaa2ee02beec7872bce107985c78fd1e2033aa
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2021-01-14 15:34:13 +0100

    Add support of RFC5939 (Capability negotiations) to liblinphone Add SalStreamConfiguration class to represent the actual and potential configuration to be offered and/or accepted in the SDP

--- src/call/call.h
+++ src/call/call.h
@@ -143,6 +143,7 @@ public:
 	const LinphoneErrorInfo *getErrorInfo () const;
 	const Address &getLocalAddress () const;
 	LinphoneCallLog *getLog () const;
+	bool supportRtcp () const;
 	RtpTransport *getMetaRtcpTransport (int streamIndex) const;
 	RtpTransport *getMetaRtpTransport (int streamIndex) const;
 	float getMicrophoneVolumeGain () const;
@@ -282,6 +283,7 @@ public:
 	
 
 	bool canSoundResourcesBeFreed () const;
+	const std::list<LinphoneMediaEncryption> getSupportedEncryptions() const;
 
 private:
 	std::shared_ptr<Participant> mParticipant;
