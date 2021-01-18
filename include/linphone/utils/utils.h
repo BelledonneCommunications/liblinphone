7a316612aed27f07ac772be5893cff98f3026675
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2021-01-18 15:09:36 +0100

    Initial commit of supported encryptions

--- include/linphone/utils/utils.h
+++ include/linphone/utils/utils.h
@@ -114,6 +114,20 @@ namespace Utils {
 		return str ? str : "";
 	}
 
+	template<class Container, class = typename std::enable_if<std::is_same<typename Container::value_type, std::string>::value>::type>
+	Container split(const std::string & str, const char delim) {
+		Container splittedStr;
+		std::size_t current, previous = 0;
+		current = str.find(delim);
+		while (current != std::string::npos) {
+			splittedStr.push_back(str.substr(previous, current - previous));
+			previous = current + 1;
+			current = str.find(delim, previous);
+		}
+		splittedStr.push_back(str.substr(previous, current - previous));
+		return splittedStr;
+	}
+
 	template<typename S, typename T>
 	inline std::string join (const std::vector<T>& elems, const S& delim) {
 		std::stringstream ss;
@@ -176,7 +190,6 @@ namespace Utils {
 	LINPHONE_PUBLIC std::string convertAnyToUtf8 (const std::string &str, const std::string &encoding);
 	LINPHONE_PUBLIC std::string quoteStringIfNotAlready(const std::string &str);
 
-	
 	class Version{
 		public:
 			LINPHONE_PUBLIC Version() = default;
