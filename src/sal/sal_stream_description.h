b94eb81ad7232d862ef80d2a41e63367c7f97fc9
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2021-01-29 15:55:38 +0100

    Integrate SalStreamConfiguration

--- src/sal/sal_stream_description.h
+++ src/sal/sal_stream_description.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2020 Belledonne Communications SARL.
+ * Copyright (c) 2010-2021 Belledonne Communications SARL.
  *
  * This file is part of Liblinphone.
  *
@@ -20,47 +20,37 @@
 #ifndef _SAL_STREAM_DESCRIPTION_H_
 #define _SAL_STREAM_DESCRIPTION_H_
 
+#include <map>
 #include <vector>
+
 #include "linphone/utils/general.h"
 #include "c-wrapper/internal/c-sal.h"
 #include "ortp/rtpsession.h"
-
-#define SAL_MEDIA_DESCRIPTION_MAX_ICE_ADDR_LEN 64
-#define SAL_MEDIA_DESCRIPTION_MAX_ICE_FOUNDATION_LEN 32
-#define SAL_MEDIA_DESCRIPTION_MAX_ICE_TYPE_LEN 6
-
-typedef struct SalIceCandidate {
-	std::string addr;
-	std::string raddr;
-	std::string foundation;
-	std::string type;
-	unsigned int componentID = 0;
-	unsigned int priority = 0;
-	int port = 0;
-	int rport = 0;
-} SalIceCandidate;
-
-typedef struct SalIceRemoteCandidate {
-	std::string addr;
-	int port = 0;
-} SalIceRemoteCandidate;
-
-/*sufficient for 256bit keys encoded in base 64*/
-#define SAL_SRTP_KEY_SIZE 128
-
-typedef struct SalSrtpCryptoAlgo {
-	unsigned int tag = 0;
-	MSCryptoSuite algo = MS_CRYPTO_SUITE_INVALID;
-	std::string master_key;
-} SalSrtpCryptoAlgo;
+#include "sal/sal_stream_configuration.h"
 
 LINPHONE_BEGIN_NAMESPACE
 
 class SalMediaDescription;
+class SalStreamConfiguration;
+class MediaSessionPrivate;
+class MS2Stream;
+class IceService;
+class SalCallOp;
+
+struct SalConfigurationCmp {
+	bool operator()(const unsigned int& lhs, const unsigned int& rhs) const;
+};
 
 class LINPHONE_PUBLIC SalStreamDescription {
+	friend class MediaSessionPrivate;
+	friend class MS2Stream;
+	friend class IceService;
+	friend class SalCallOp;
+	friend class SalMediaDescription;
 
 	public:
+
+		static unsigned int actualConfigurationIndex;
 
 		SalStreamDescription();
 		SalStreamDescription(const SalMediaDescription * salMediaDesc, const belle_sdp_media_description_t *media_desc);
@@ -73,6 +63,23 @@ class LINPHONE_PUBLIC SalStreamDescription {
 		belle_sdp_media_description_t * toSdpMediaDescription(const SalMediaDescription * salMediaDesc, belle_sdp_session_description_t *session_desc) const;
 		bool enabled() const;
 		void disable();
+
+		// This function returns the index of the chosen configuration for this stream. It should be called only if capability negotiation functionality described in rfc5939 is supported.
+		// It returns 0 if the current chosen configuration is the actual configuration or if rfc5939 is not supported.
+		// On the offerer side, its value is valid only if it received the answer to it offer
+		// On the answerer side, it is valid only after it has chosen the valid configuration.
+		// Returned values:
+		// - 0: actual configuration
+		// - 1 to 2^31-1: configuration number as received in the SDP acfg or pcfg attribute
+		const unsigned int & getChosenConfigurationIndex() const;
+		const unsigned int & getActualConfigurationIndex() const;
+
+		const SalStreamConfiguration & getConfigurationAtIndex(const int & index) const;
+		const SalStreamConfiguration & getActualConfiguration() const;
+		const SalStreamConfiguration & getChosenConfiguration() const;
+
+		void addActualConfiguration(const SalStreamConfiguration & cfg);
+		void addConfigurationAtIndex(const unsigned int & idx, const SalStreamConfiguration & cfg);
 
 		/*these are switch case, so that when a new proto is added we can't forget to modify this function*/
 		bool hasAvpf() const;
@@ -89,72 +96,61 @@ class LINPHONE_PUBLIC SalStreamDescription {
 		const std::string & getRtpAddress() const;
 		const int & getRtpPort() const;
 		const std::list<PayloadType*> & getPayloads() const;
+		bool isBundleOnly() const;
 
 		const SalStreamType & getType() const;
 		const std::string getTypeAsString() const;
+		void setProto(const SalMediaProto & newProto);
 		const SalMediaProto & getProto() const;
 		const std::string getProtoAsString() const;
 		SalStreamDir getDirection() const;
+		const int & getMaxRate() const;
+		SalCustomSdpAttribute * getCustomSdpAttributes() const;
+
+		void setZrtpHash(const uint8_t enable, uint8_t* zrtphash = NULL);
+
+		const std::vector<SalSrtpCryptoAlgo> & getCryptos() const;
+		const SalSrtpCryptoAlgo & getCryptoAtIndex(const size_t & idx) const;
 
 		std::string name; /*unique name of stream, in order to ease offer/answer model algorithm*/
-		SalMediaProto proto = SalProtoRtpAvp;
 		SalStreamType type = SalAudio;
 		std::string typeother;
-		std::string proto_other;
 		std::string rtp_addr;
 		std::string rtcp_addr;
-		unsigned int rtp_ssrc = 0;
-		std::string rtcp_cname;
 		int rtp_port = 0;
 		int rtcp_port = 0;
-		std::list<PayloadType*> payloads; /**<list of PayloadType */
 		std::list<PayloadType*> already_assigned_payloads; /**<list of PayloadType offered in the past, used for correct allocation of payload type numbers*/
 		int bandwidth = 0;
-		int ptime = 0;
-		int maxptime = 0;
-		SalStreamDir dir = SalStreamInactive;
-		std::vector<SalSrtpCryptoAlgo> crypto;
-		unsigned int crypto_local_tag = 0;
-		int max_rate = 0;
-		bool bundle_only = false;
-		bool implicit_rtcp_fb = false;
-		bool pad[2]; /* Use me */
-		OrtpRtcpFbConfiguration rtcp_fb;
-		OrtpRtcpXrConfiguration rtcp_xr;
-		SalCustomSdpAttribute *custom_sdp_attributes = nullptr;
-		std::vector<SalIceCandidate> ice_candidates;
-		std::vector<SalIceRemoteCandidate> ice_remote_candidates;
-		std::string ice_ufrag;
-		std::string ice_pwd;
-		std::string mid; /* Media line identifier for RTP bundle mode */
-		int mid_rtp_ext_header_id = 0; /* Identifier for the MID field in the RTP extension header */
-		bool ice_mismatch = false;
-		bool set_nortpproxy = false; /*Formely set by ICE to indicate to the proxy that it has nothing to do*/
-		bool rtcp_mux = false;
-		uint8_t haveZrtpHash = 0; /**< flag for zrtp hash presence */
-		uint8_t haveLimeIk = 0; /**< flag for lime Ik presence */
-		uint8_t zrtphash[128];
-		std::string dtls_fingerprint;
-		SalDtlsRole dtls_role = SalDtlsRoleInvalid;
-		int ttl = 0; /*for multicast -1 to disable*/
 		SalMulticastRole multicast_role = SalMulticastInactive;
 
 	private:
-		bool isRecvOnly(const PayloadType *p) const;
-		bool isSamePayloadType(const PayloadType *p1, const PayloadType *p2) const;
-		bool isSamePayloadList(const std::list<PayloadType*> & l1, const std::list<PayloadType*> & l2) const;
 
-		void addIceRemoteCandidatesToSdp(belle_sdp_media_description_t *md) const;
-		void addIceCandidatesToSdp(belle_sdp_media_description_t *md) const;
-		void addRtcpFbAttributesToSdp(belle_sdp_media_description_t *media_desc) const;
-		bool_t isRtcpFbTrrIntTheSameForAllPayloads(uint16_t *trr_int) const;
-		void addMidAttributesToSdp(belle_sdp_media_description_t *media_desc) const;
-		void applyRtcpFbAttributeToPayload(belle_sdp_rtcp_fb_attribute_t *fb_attribute, PayloadType *pt);
-		bool_t sdpParseRtcpFbParameters(const belle_sdp_media_description_t *media_desc);
-		void sdpParsePayloadTypes(const belle_sdp_media_description_t *media_desc);
-		void sdpParseMediaCryptoParameters(const belle_sdp_media_description_t *media_desc);
-		void sdpParseMediaIceParameters(const belle_sdp_media_description_t *media_desc);
-		void enableAvpfForStream();
+		unsigned int cfgIndex;
+
+		// Map of the available configurations
+		std::map<unsigned int, SalStreamConfiguration, SalConfigurationCmp> cfgs;
+
+		void createActualCfg(const SalMediaDescription * salMediaDesc, const belle_sdp_media_description_t *media_desc);
+		void setProtoInCfg(SalStreamConfiguration & cfg, const std::string & str);
+
+		void setupRtcpFb(const bool nackEnabled, const bool tmmbrEnabled, const bool implicitRtcpFb);
+		void setupRtcpXr(const OrtpRtcpXrConfiguration & rtcpXr);
+		void setDirection(const SalStreamDir & newDir);
+		void setBundleOnly(const bool enable);
+		void setDtls(const SalDtlsRole role, const std::string & fingerprint = std::string());
+		void setPtime(const int & ptime = -1, const int & maxptime = -1);
+		void setCrypto(const size_t & idx, const SalSrtpCryptoAlgo & newCrypto);
+
+		void addIceRemoteCandidatesToSdp(const SalStreamConfiguration & cfg, belle_sdp_media_description_t *md) const;
+		void addIceCandidatesToSdp(const SalStreamConfiguration & cfg, belle_sdp_media_description_t *md) const;
+		void addRtcpFbAttributesToSdp(const SalStreamConfiguration & cfg, belle_sdp_media_description_t *media_desc) const;
+		bool isRtcpFbTrrIntTheSameForAllPayloads(const SalStreamConfiguration & cfg, uint16_t *trr_int) const;
+		void addMidAttributesToSdp(const SalStreamConfiguration & cfg, belle_sdp_media_description_t *media_desc) const;
+		void applyRtcpFbAttributeToPayload(SalStreamConfiguration & cfg, belle_sdp_rtcp_fb_attribute_t *fb_attribute, PayloadType *pt);
+		bool sdpParseRtcpFbParameters(SalStreamConfiguration & cfg, const belle_sdp_media_description_t *media_desc);
+		void sdpParsePayloadTypes(SalStreamConfiguration & cfg, const belle_sdp_media_description_t *media_desc) const;
+		void sdpParseMediaCryptoParameters(SalStreamConfiguration & cfg, const belle_sdp_media_description_t *media_desc) const;
+		void sdpParseMediaIceParameters(SalStreamConfiguration & cfg, const belle_sdp_media_description_t *media_desc);
 
 };
 
