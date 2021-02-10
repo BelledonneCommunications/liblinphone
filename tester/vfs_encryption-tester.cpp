20fcf1666a9c901831994781a1204f27de9ac4b7
Julien Wadel <julien.wadel@belledonne-communications.com>
2021-02-11 00:11:52 +0100

    - reset to 10 parallelized process for Windows - add some pointers test on testers
    - Add missing assert/end

--- tester/vfs_encryption-tester.cpp
+++ tester/vfs_encryption-tester.cpp
@@ -467,11 +467,13 @@ static void file_transfer_test(const uint16_t encryptionModule, const char *rand
 	participantsAddresses = bctbx_list_append(participantsAddresses, linphone_address_new(linphone_core_get_identity(pauline->lc)));
 	const char *initialSubject = "Colleagues";
 	LinphoneChatRoom *marieCr = create_chat_room_client_side(coresList, marie, &initialMarieStats, participantsAddresses, initialSubject, TRUE);
+	if(!BC_ASSERT_PTR_NOT_NULL(marieCr)) goto end;
 	const LinphoneAddress *confAddr = linphone_chat_room_get_conference_address(marieCr);
 
 	// Check that the chat room is correctly created on Pauline's side and that the participants are added
 	LinphoneChatRoom *paulineCr = check_creation_chat_room_client_side(coresList, pauline, &initialPaulineStats, confAddr, initialSubject, 1, FALSE);
 
+	if(!BC_ASSERT_PTR_NOT_NULL(paulineCr)) goto end;
 	// send file
 	char *sendFilepath = bc_tester_res("sounds/sintel_trailer_opus_vp8.mkv");
 	_send_file(marieCr, sendFilepath, nullptr, false);
@@ -491,6 +493,7 @@ static void file_transfer_test(const uint16_t encryptionModule, const char *rand
 
 	// Get the file using the linphone content API
 	LinphoneChatMessage *msg = pauline->stat.last_received_chat_message;
+	if(!BC_ASSERT_PTR_NOT_NULL(msg)) goto end;
 	auto contents = linphone_chat_message_get_contents(msg);
 	BC_ASSERT_PTR_NOT_NULL(contents);
 	BC_ASSERT_EQUAL(1, (int)bctbx_list_size(contents), int, "%d");
@@ -509,10 +512,12 @@ static void file_transfer_test(const uint16_t encryptionModule, const char *rand
 		std::remove(plainFilePath);
 		bctbx_free(plainFilePath);
 	}
-
+end:
 	// cleaning
-	linphone_core_manager_delete_chat_room(marie, marieCr, coresList);
-	linphone_core_manager_delete_chat_room(pauline, paulineCr, coresList);
+	if(marieCr)
+		linphone_core_manager_delete_chat_room(marie, marieCr, coresList);
+	if(paulineCr)
+		linphone_core_manager_delete_chat_room(pauline, paulineCr, coresList);
 	remove(receivePaulineFilepath);
 	bc_free(sendFilepath);
 	bc_free(receivePaulineFilepath);
