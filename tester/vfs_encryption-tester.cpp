7f95f7adeefed778a930741d62ca806b489331e3
Julien Wadel <julien.wadel@belledonne-communications.com>
2021-02-11 09:37:25 +0100

    Add missing assert/end

--- tester/vfs_encryption-tester.cpp
+++ tester/vfs_encryption-tester.cpp
@@ -467,11 +467,13 @@ static void file_transfer_test(const uint16_t encryptionModule, const char *rand
 	participantsAddresses = bctbx_list_append(participantsAddresses, linphone_address_new(linphone_core_get_identity(pauline->lc)));
 	const char *initialSubject = "Colleagues";
 	LinphoneChatRoom *marieCr = create_chat_room_client_side(coresList, marie, &initialMarieStats, participantsAddresses, initialSubject, TRUE);
+	if(!BC_ASSERT_PTR_NOT_NULL(marieCr)) goto end;
 	const LinphoneAddress *confAddr = linphone_chat_room_get_conference_address(marieCr);
 
 	// Check that the chat room is correctly created on Pauline's side and that the participants are added
 	LinphoneChatRoom *paulineCr = check_creation_chat_room_client_side(coresList, pauline, &initialPaulineStats, confAddr, initialSubject, 1, FALSE);
 
+	if(!BC_ASSERT_PTR_NOT_NULL(paulineCr)) goto end;
 	// send file
 	char *sendFilepath = bc_tester_res("sounds/sintel_trailer_opus_vp8.mkv");
 	_send_file(marieCr, sendFilepath, nullptr, false);
