080f2f1c2b1f39f46efff01e1243071e225d9d89
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2021-03-04 12:09:08 +0100

    Do not change local params encryption

--- src/conference/session/media-session-p.h
+++ src/conference/session/media-session-p.h
@@ -155,6 +155,9 @@ public:
 	void setState (CallSession::State newState, const std::string &message) override;
 
 	LinphoneMediaEncryption getEncryptionFromMediaDescription(const std::shared_ptr<SalMediaDescription> & md) const;
+
+	LinphoneMediaEncryption getNegotiatedMediaEncryption() const;
+
 private:
 	/* IceServiceListener methods:*/
 	virtual void onGatheringFinished(IceService &service) override;
@@ -167,6 +170,7 @@ private:
 #endif // ifdef TEST_EXT_RENDERER
 	static int sendDtmf (void *data, unsigned int revents);
 
+	bool incompatibleSecurity(const std::shared_ptr<SalMediaDescription> &md) const;
 	
 
 	void assignStreamsIndexesIncoming(const std::shared_ptr<SalMediaDescription> & md);
@@ -188,7 +192,7 @@ private:
 	void forceStreamsDirAccordingToState (std::shared_ptr<SalMediaDescription> & md);
 	bool generateB64CryptoKey (size_t keyLength, std::string & keyOut, size_t keyOutSize) const;
 	void makeLocalStreamDecription(std::shared_ptr<SalMediaDescription> & md, const bool enabled, const std::string name, const size_t & idx, const SalStreamType type, const SalMediaProto proto, const SalStreamDir dir, const std::list<OrtpPayloadType*> & codecs, const std::string mid, const bool & multicastEnabled, const int & ttl, const SalCustomSdpAttribute *customSdpAttributes);
-	void makeLocalMediaDescription (bool localIsOfferer, const bool supportsCapabilityNegotiationAttributes);
+	void makeLocalMediaDescription (bool localIsOfferer, const bool supportsCapabilityNegotiationAttributes, const bool isCapabilityNegotiationReInvite = false);
 	void setupDtlsKeys (std::shared_ptr<SalMediaDescription> & md);
 	void setupEncryptionKeys (std::shared_ptr<SalMediaDescription> & md);
 	void setupRtcpFb (std::shared_ptr<SalMediaDescription> & md);
@@ -241,8 +245,8 @@ private:
 	void stunAuthRequestedCb (const char *realm, const char *nonce, const char **username, const char **password, const char **ha1);
 	Stream *getStream(LinphoneStreamType type)const;
 	int portFromStreamIndex(int index);
-	SalMediaProto getAudioProto();
-	SalMediaProto getAudioProto(const std::shared_ptr<SalMediaDescription> remote_md);
+	SalMediaProto getAudioProto(const bool useCurrentParams) const;
+	SalMediaProto getAudioProto(const std::shared_ptr<SalMediaDescription> remote_md, const bool useCurrentParams) const;
 	bool hasAvpf(const std::shared_ptr<SalMediaDescription> & md)const;
 	void queueIceGatheringTask(const std::function<void()> &lambda);
 	void runIceGatheringTasks();
@@ -260,6 +264,8 @@ private:
 	int mainAudioStreamIndex = -1;
 	int mainVideoStreamIndex = -1;
 	int mainTextStreamIndex = -1;
+
+	LinphoneMediaEncryption negotiatedEncryption = LinphoneMediaEncryptionNone;
 
 	LinphoneNatPolicy *natPolicy = nullptr;
 	std::unique_ptr<StunClient> stunClient;
