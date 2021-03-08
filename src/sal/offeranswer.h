425acf785e670c6a84c2ed0ff3fb19e180e46302
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2021-03-08 09:05:00 +0100

    Rename bool_t as bool

--- src/sal/offeranswer.h
+++ src/sal/offeranswer.h
@@ -54,22 +54,22 @@ class OfferAnswerEngine {
 		 * The returned media description is an answer and should be sent to the offerer.
 		**/
 		static std::shared_ptr<SalMediaDescription> initiateIncoming(MSFactory* factory, const std::shared_ptr<SalMediaDescription> local_capabilities,
-							std::shared_ptr<SalMediaDescription> remote_offer, bool_t one_matching_codec);
+							std::shared_ptr<SalMediaDescription> remote_offer, bool one_matching_codec);
 
 	private:
 
-		static bool_t onlyTelephoneEvent(const std::list<OrtpPayloadType*> & l);
+		static bool onlyTelephoneEvent(const std::list<OrtpPayloadType*> & l);
 		static bool areProtoInStreamCompatibles(const SalStreamDescription & localStream, const SalStreamDescription & otherStream);
 		static bool areProtoCompatibles(SalMediaProto localProto, SalMediaProto otherProto);
 		static SalStreamDir computeDirIncoming(SalStreamDir local, SalStreamDir offered);
 		static SalStreamDir computeDirOutgoing(SalStreamDir local, SalStreamDir answered);
-		static bool_t matchCryptoAlgo(const std::vector<SalSrtpCryptoAlgo> &local, const std::vector<SalSrtpCryptoAlgo> &remote, SalSrtpCryptoAlgo & result, unsigned int & choosen_local_tag, bool_t use_local_key);
-		static std::list<OrtpPayloadType*> matchPayloads(MSFactory *factory, const std::list<OrtpPayloadType*> & local, const std::list<OrtpPayloadType*> & remote, bool_t reading_response, bool_t one_matching_codec);
+		static bool matchCryptoAlgo(const std::vector<SalSrtpCryptoAlgo> &local, const std::vector<SalSrtpCryptoAlgo> &remote, SalSrtpCryptoAlgo & result, unsigned int & choosen_local_tag, bool use_local_key);
+		static std::list<OrtpPayloadType*> matchPayloads(MSFactory *factory, const std::list<OrtpPayloadType*> & local, const std::list<OrtpPayloadType*> & remote, bool reading_response, bool one_matching_codec);
 		static PayloadType * genericMatch(const std::list<OrtpPayloadType*> & local_payloads, const PayloadType *refpt, const std::list<OrtpPayloadType*> & remote_payloads);
-		static PayloadType * findPayloadTypeBestMatch(MSFactory *factory, const std::list<OrtpPayloadType*> & local_payloads, const PayloadType *refpt, const std::list<OrtpPayloadType*> & remote_payloads, bool_t reading_response);
+		static PayloadType * findPayloadTypeBestMatch(MSFactory *factory, const std::list<OrtpPayloadType*> & local_payloads, const PayloadType *refpt, const std::list<OrtpPayloadType*> & remote_payloads, bool reading_response);
 
-		static SalStreamDescription initiateIncomingStream(MSFactory *factory, const SalStreamDescription & local_cap, const SalStreamDescription & remote_offer, bool_t one_matching_codec, const std::string &bundle_owner_mid, const bool allowCapabilityNegotiation);
-		static std::pair<SalStreamConfiguration, bool> initiateIncomingConfiguration(MSFactory *factory, const SalStreamDescription & local_cap, const SalStreamDescription & remote_offer, const SalStreamDescription & result, bool_t one_matching_codec, const std::string &bundle_owner_mid, const bellesip::SDP::SDPPotentialCfgGraph::media_description_config::key_type & localCfgIdx, const bellesip::SDP::SDPPotentialCfgGraph::media_description_config::key_type & remoteCfgIdx);
+		static SalStreamDescription initiateIncomingStream(MSFactory *factory, const SalStreamDescription & local_cap, const SalStreamDescription & remote_offer, bool one_matching_codec, const std::string &bundle_owner_mid, const bool allowCapabilityNegotiation);
+		static std::pair<SalStreamConfiguration, bool> initiateIncomingConfiguration(MSFactory *factory, const SalStreamDescription & local_cap, const SalStreamDescription & remote_offer, const SalStreamDescription & result, bool one_matching_codec, const std::string &bundle_owner_mid, const bellesip::SDP::SDPPotentialCfgGraph::media_description_config::key_type & localCfgIdx, const bellesip::SDP::SDPPotentialCfgGraph::media_description_config::key_type & remoteCfgIdx);
 
 		static SalStreamDescription initiateOutgoingStream(MSFactory* factory, const SalStreamDescription & local_offer, const SalStreamDescription & remote_answer, const bool allowCapabilityNegotiation);
 
