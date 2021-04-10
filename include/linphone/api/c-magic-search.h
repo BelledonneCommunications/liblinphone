4f8a5f2eee753603146b0d1ec3ad44b0861593c0
Julien Wadel <julien.wadel@belledonne-communications.com>
2021-04-10 15:39:21 +0200

    - Remove SASL - Use linphone callback for Magic search - Multithreading LDAP search in cpp - Multithreading and queue management on Magic search requests - LDAP : Tester recfiles - LDAP : Adapt tests
    # Conflicts:
    #	tester/rcfiles/empty_rc
    #	tester/setup_tester.c

--- include/linphone/api/c-magic-search.h
+++ include/linphone/api/c-magic-search.h
@@ -20,6 +20,7 @@
 #ifndef _L_C_MAGIC_SEARCH_H_
 #define _L_C_MAGIC_SEARCH_H_
 
+#include "linphone/api/c-magic-search-cbs.h"
 #include "linphone/api/c-types.h"
 
 // =============================================================================
@@ -52,6 +53,28 @@ LINPHONE_PUBLIC LinphoneMagicSearch *linphone_magic_search_ref (LinphoneMagicSea
  * @param magic_search a #LinphoneMagicSearch object @notnil
  **/
 LINPHONE_PUBLIC void linphone_magic_search_unref (LinphoneMagicSearch *magic_search);
+
+/**
+ * Add a listener in order to be notified of #LinphoneMagicSearch events.
+ * @param magic_search #LinphoneMagicSearch object to monitor. @notnil
+ * @param cbs A #LinphoneMagicSearchCbs object holding the callbacks you need. @notnil
+ */
+LINPHONE_PUBLIC void linphone_magic_search_add_callbacks(LinphoneMagicSearch *magic_search, LinphoneMagicSearchCbs *cbs);
+
+/**
+ * Remove a listener from a #LinphoneMagicSearch
+ * @param magic_search #LinphoneMagicSearch object @notnil
+ * @param cbs #LinphoneMagicSearchCbs object to remove. @notnil
+ */
+LINPHONE_PUBLIC void linphone_magic_search_remove_callbacks(LinphoneMagicSearch *magic_search, LinphoneMagicSearchCbs *cbs);
+
+/**
+ * Gets the current LinphoneMagicSearchCbs.
+ * This is meant only to be called from a callback to be able to get the user_data associated with the #LinphoneMagicSearchCbs that is calling the callback.
+ * @param magic_search #LinphoneMagicSearch object @notnil
+ * @return The #LinphoneMagicSearchCbs that has called the last callback. @maybenil
+ */
+LINPHONE_PUBLIC LinphoneMagicSearchCbs *linphone_magic_search_get_current_callbacks(const LinphoneMagicSearch *magic_search);
 
 /**
  * Set the minimum value used to calculate the weight in search
@@ -158,10 +181,37 @@ LINPHONE_PUBLIC void linphone_magic_search_reset_search_cache (LinphoneMagicSear
  * @return sorted list of \bctbx_list{LinphoneSearchResult} @tobefreed @notnil
  **/
 LINPHONE_PUBLIC bctbx_list_t *linphone_magic_search_get_contact_list_from_filter (
-	const LinphoneMagicSearch *magic_search,
+	LinphoneMagicSearch *magic_search,
 	const char *filter,
 	const char *domain
 );
+
+/**
+ * Create a sorted list of SearchResult asynchronous from SipUri, Contact name,
+ * Contact displayname, Contact phone number, which match with a filter word
+ * The last item list will be an address formed with "filter" if a proxy config exist
+ * During the first search, a cache is created and used for the next search
+ * Use linphone_magic_search_reset_search_cache() to begin a new search
+ * @param magic_search a #LinphoneMagicSearch object @notnil
+ * @param filter word we search @maybenil
+ * @param domain domain which we want to search only @maybenil
+ ** NULL or "" for searching in all contact
+ ** "*" for searching in contact with sip SipUri
+ ** "yourdomain" for searching in contact from "yourdomain" domain
+ **/
+//typedef void (MagicSearchCb)(const void *, void *);
+LINPHONE_PUBLIC void linphone_magic_search_get_contact_list_from_filter_async (
+	LinphoneMagicSearch *magic_search,
+	const char *filter,
+	const char *domain
+);
+/**
+ * 
+ * @param magic_search a #LinphoneMagicSearch object @notnil
+ * @return sorted list of \bctbx_list{LinphoneSearchResult} @tobefreed @notnil
+ * 
+ **/
+LINPHONE_PUBLIC bctbx_list_t* linphone_magic_search_get_last_search(const LinphoneMagicSearch *magic_search);
 
 /**
  * @}
