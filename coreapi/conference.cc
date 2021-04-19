c17c70c2dfc3ab532eb7e2273b43474e22bdf4ff
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2021-04-19 14:53:02 +0200

    Set conference to call before sendign SIP messages to add it to conference

--- coreapi/conference.cc
+++ coreapi/conference.cc
@@ -617,6 +617,7 @@ bool LocalConference::addParticipant (std::shared_ptr<LinphonePrivate::Call> cal
 					L_GET_PRIVATE(call->getParams()))->setInConference(true);
 				const_cast<LinphonePrivate::MediaSessionParamsPrivate *>(
 					L_GET_PRIVATE(call->getParams()))->setConferenceId(confId);
+				call->setConference(toC());
 			break;
 			case LinphoneCallPaused:
 			{
@@ -631,6 +632,7 @@ bool LocalConference::addParticipant (std::shared_ptr<LinphonePrivate::Call> cal
 				const_cast<LinphonePrivate::MediaSessionParams *>(
 					call->getParams())->enableVideo(getCurrentParams().videoEnabled());
 				// Conference resumes call that previously paused in order to add the participant
+				call->setConference(toC());
 				call->resume();
 			}
 			break;
@@ -641,6 +643,8 @@ bool LocalConference::addParticipant (std::shared_ptr<LinphonePrivate::Call> cal
 				linphone_call_params_set_in_conference(params, TRUE);
 				linphone_call_params_enable_video(params, getCurrentParams().videoEnabled());
 				linphone_call_params_set_conference_id(params, confId.c_str());
+
+				call->setConference(toC());
 
 				linphone_call_update(call->toC(), params);
 				linphone_call_params_unref(params);
