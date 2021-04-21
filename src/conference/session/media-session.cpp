193db236b7cc44944d8ee7d577f88afab917f5da
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2021-04-21 11:47:16 +0200

    Fix cast error

--- src/conference/session/media-session.cpp
+++ src/conference/session/media-session.cpp
@@ -1433,7 +1433,7 @@ void MediaSessionPrivate::makeLocalMediaDescription(bool localIsOfferer, const b
 		if (audioStreamIdx == -1) {
 			md->streams.push_back(audioStream);
 		} else {
-			md->streams[audioStreamIdx] = audioStream;
+			md->streams[static_cast<decltype(md->streams)::size_type>(audioStreamIdx)] = audioStream;
 		}
 		PayloadTypeHandler::clearPayloadList(audioCodecs);
 	}
@@ -1454,7 +1454,7 @@ void MediaSessionPrivate::makeLocalMediaDescription(bool localIsOfferer, const b
 		if (videoStreamIdx == -1) {
 			md->streams.push_back(videoStream);
 		} else {
-			md->streams[videoStreamIdx] = videoStream;
+			md->streams[static_cast<decltype(md->streams)::size_type>(videoStreamIdx)] = videoStream;
 		}
 		PayloadTypeHandler::clearPayloadList(videoCodecs);
 	}
@@ -1474,7 +1474,7 @@ void MediaSessionPrivate::makeLocalMediaDescription(bool localIsOfferer, const b
 		if (textStreamIdx == -1) {
 			md->streams.push_back(textStream);
 		} else {
-			md->streams[textStreamIdx] = textStream;
+			md->streams[static_cast<decltype(md->streams)::size_type>(textStreamIdx)] = textStream;
 		}
 		PayloadTypeHandler::clearPayloadList(textCodecs);
 	}
