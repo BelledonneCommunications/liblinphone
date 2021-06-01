44b23b9df5270affdd4a5c142357433e5f5566c2
Julien Wadel <julien.wadel@belledonne-communications.com>
2021-06-01 11:21:27 +0200

    Fix setting output audio device that was based on ringer device on specific call state.
    - On incoming, set audio devices from default
    - Remove setOutputAudioDevice from tonemanager
    
    - Reset sound cards when setting use_files

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -869,7 +869,10 @@ const list<AudioDevice *> Core::getExtendedAudioDevices() const {
 
 void Core::setInputAudioDevice(AudioDevice *audioDevice) {
 	L_D();
-	
+	if(getCCore()->use_files) {
+		lInfo() << "Trying to change input audio device on core while use_files mode is on : do nothing";
+		return;
+	}
 	bool success = d->setInputAudioDevice(audioDevice);
 
 	if (success) {
@@ -879,7 +882,10 @@ void Core::setInputAudioDevice(AudioDevice *audioDevice) {
 
 void Core::setOutputAudioDevice(AudioDevice *audioDevice) {
 	L_D();
-
+	if(getCCore()->use_files) {
+		lInfo() << "Trying to change output audio device on core while use_files mode is on : do nothing";
+		return;
+	}
 	bool success = d->setOutputAudioDevice(audioDevice);
 
 	if (success) {
@@ -1002,13 +1008,21 @@ void Core::setInputAudioDeviceBySndCard(MSSndCard *card){
 
 
 AudioDevice* Core::getDefaultInputAudioDevice() const {
-	MSSndCard *card = getCCore()->sound_conf.capt_sndcard;
-	return findAudioDeviceMatchingMsSoundCard(card);
+	if(getCCore()->use_files)
+		return nullptr;
+	else{
+		MSSndCard *card = getCCore()->sound_conf.capt_sndcard;
+		return findAudioDeviceMatchingMsSoundCard(card);
+	}
 }
 
 AudioDevice* Core::getDefaultOutputAudioDevice() const {
-	MSSndCard *card = getCCore()->sound_conf.play_sndcard;
-	return findAudioDeviceMatchingMsSoundCard(card);
+	if(getCCore()->use_files)
+		return nullptr;
+	else{
+		MSSndCard *card = getCCore()->sound_conf.play_sndcard;
+		return findAudioDeviceMatchingMsSoundCard(card);
+	}
 }
 
 // -----------------------------------------------------------------------------
