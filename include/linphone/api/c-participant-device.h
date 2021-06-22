7d6547976e6ce50af444098564b02e4b32597879
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2021-06-22 17:21:39 +0200

    Renamed linphone_participant_device_get/set_window_id + added Java wrapper for it

--- include/linphone/api/c-participant-device.h
+++ include/linphone/api/c-participant-device.h
@@ -130,14 +130,14 @@ LINPHONE_PUBLIC uint32_t linphone_participant_device_get_ssrc(const LinphonePart
  * @param participant_device A #LinphoneParticipantDevice object @notnil
  * @param window_id the window ID of the device @maybenil
  */
-LINPHONE_PUBLIC void linphone_participant_device_set_window_id(LinphoneParticipantDevice *participant_device, void * window_id);
+LINPHONE_PUBLIC void linphone_participant_device_set_native_video_window_id(LinphoneParticipantDevice *participant_device, void * window_id);
 
 /**
  * Get window ID.
  * @param participant_device A #LinphoneParticipantDevice object @notnil
  * @return the window ID of the device @maybenil
  */
-LINPHONE_PUBLIC void * linphone_participant_device_get_window_id(const LinphoneParticipantDevice *participant_device);
+LINPHONE_PUBLIC void * linphone_participant_device_get_native_video_window_id(const LinphoneParticipantDevice *participant_device);
 
 /**
  * @}
