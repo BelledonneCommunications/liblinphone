3fe1769aa94b52c671bc553f3c3e72b21b541914
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2021-06-23 10:34:58 +0200

    Added AndroidPlatformHelpers tools for ParticipantDevice video window id

--- coreapi/linphonecore.c
+++ coreapi/linphonecore.c
@@ -6342,7 +6342,7 @@ void _linphone_core_set_native_video_window_id(LinphoneCore *lc, void *id) {
 }
 
 void linphone_core_set_native_video_window_id(LinphoneCore *lc, void *id) {
-#ifdef ANDROID
+#ifdef __ANDROID__
 	getPlatformHelpers(lc)->setVideoWindow(id);
 #else
 	_linphone_core_set_native_video_window_id(lc, id);
@@ -6382,7 +6382,7 @@ void _linphone_core_set_native_preview_window_id(LinphoneCore *lc, void *id) {
 }
 
 void linphone_core_set_native_preview_window_id(LinphoneCore *lc, void *id) {
-#ifdef ANDROID
+#ifdef __ANDROID__
 	getPlatformHelpers(lc)->setVideoPreviewWindow(id);
 #else
 	_linphone_core_set_native_preview_window_id(lc, id);
