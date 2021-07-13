a221386ffe797a8d5fe5d7e1f5528f398ccf6ca2
Julien Wadel <julien.wadel@belledonne-communications.com>
2021-07-13 18:55:35 +0100

    - Stop audio stream when setting new device and Filter don't implement MS_AUDIO_CAPTURE_SET_INTERNAL_ID and MS_AUDIO_PLAYBACK_SET_INTERNAL_ID. This fix allow changing device on Desktop while ringback. - Add Audio stream stop count for tests.

--- tester/liblinphone_tester.h
+++ tester/liblinphone_tester.h
@@ -570,6 +570,7 @@ int check_nb_media_starts(unsigned int media_type, LinphoneCoreManager *caller, 
 
 void setup_sdp_handling(const LinphoneCallTestParams* params, LinphoneCoreManager* mgr);
 void check_stream_encryption(LinphoneCall * call);
+int get_stream_stop_count(LinphoneCall * call);
 bool_t search_matching_srtp_suite(LinphoneCoreManager* caller_mgr,LinphoneCoreManager* callee_mgr);
 
 LinphoneChatRoom * create_chat_room_client_side(bctbx_list_t *lcs, LinphoneCoreManager *lcm, stats *initialStats, bctbx_list_t *participantsAddresses, const char* initialSubject, bool_t encrypted);
