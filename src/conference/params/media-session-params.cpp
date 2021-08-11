a5a4beabeea323341090039a2a149ffaeb50f671
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2021-08-11 10:53:42 +0200

    Fix failure of test 'Simple conference with participant addition from not admin'

--- src/conference/params/media-session-params.cpp
+++ src/conference/params/media-session-params.cpp
@@ -336,6 +336,11 @@ void MediaSessionParams::setAudioBandwidthLimit (int value) {
 	d->audioBandwidthLimit = value;
 }
 
+void MediaSessionParams::setAudioDirection (SalStreamDir direction) {
+	L_D();
+	d->audioDirection = MediaSessionParamsPrivate::salStreamDirToMediaDirection(direction);
+}
+
 void MediaSessionParams::setAudioDirection (LinphoneMediaDirection direction) {
 	L_D();
 	d->audioDirection = direction;
@@ -388,6 +393,11 @@ LinphonePayloadType * MediaSessionParams::getUsedVideoPayloadType () const {
 LinphoneMediaDirection MediaSessionParams::getVideoDirection () const {
 	L_D();
 	return d->videoDirection;
+}
+
+void MediaSessionParams::setVideoDirection (SalStreamDir direction) {
+	L_D();
+	d->videoDirection = MediaSessionParamsPrivate::salStreamDirToMediaDirection(direction);
 }
 
 void MediaSessionParams::setVideoDirection (LinphoneMediaDirection direction) {
