240a28e29f50cda4f950f19ac85bdf800b2c8f29
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2021-08-30 11:33:54 +0200

    Fix compile errors and crash of Local Conference Tester suite

--- src/conference/handlers/remote-conference-event-handler.cpp
+++ src/conference/handlers/remote-conference-event-handler.cpp
@@ -302,9 +302,11 @@ void RemoteConferenceEventHandler::conferenceInfoNotifyReceived (const string &x
 							}
 						} else if (mediaType.compare("video") == 0) {
 							device->setVideoDirection(mediaDirection);
-							const std::string label = media.getLabel().get();
-							if (!label.empty()) {
-								device->setLabel(label);
+							if (media.getLabel()) {
+								const std::string label = media.getLabel().get();
+								if (!label.empty()) {
+									device->setLabel(label);
+								}
 							}
 						} else if (mediaType.compare("text") == 0) {
 							device->setTextDirection(mediaDirection);
