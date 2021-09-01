7b1afaa572aca1b142cbe6e4e2079542e99a019a
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2021-09-01 16:29:02 +0200

    Change contact address if call in IncomingReceived state is added to conference Add test to terminate conference by having core hosting the conference ending calls one by one Exit calls from conference when they reach state LinphoneCallEnd because 200Ok of BYE may take a long time

--- src/conference/session/call-session.cpp
+++ src/conference/session/call-session.cpp
@@ -129,7 +129,6 @@ void CallSessionPrivate::setState (CallSession::State newState, const string &me
 					default:
 						break;
 				}
-				setTerminated();
 				break;
 			case CallSession::State::Connected:
 				log->status = LinphoneCallSuccess;
@@ -148,6 +147,8 @@ void CallSessionPrivate::setState (CallSession::State newState, const string &me
 
 		if (newState == CallSession::State::Released) {
 			setReleased(); /* Shall be performed after app notification */
+		} else if ((newState == CallSession::State::End) || (newState == CallSession::State::Error)) {
+			setTerminated();
 		}
 	}
 }
