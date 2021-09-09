5853025d5414bc50b4087a61146f7d7eeb99915f
Mickaël Turnel <mickael.turnel@belledonne-communications.com>
2021-09-09 12:10:26 +0200

    Add ICS support and conference invitations

--- tester/liblinphone_tester.h
+++ tester/liblinphone_tester.h
@@ -59,6 +59,7 @@ extern test_suite_t conference_event_test_suite;
 extern test_suite_t conference_test_suite;
 extern test_suite_t contents_test_suite;
 extern test_suite_t cpim_test_suite;
+extern test_suite_t ics_test_suite;
 extern test_suite_t dtmf_test_suite;
 extern test_suite_t event_test_suite;
 extern test_suite_t main_db_test_suite;
