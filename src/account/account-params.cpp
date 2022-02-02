c21e3a9c7aa078de463313198f2bf4e1980a0c61
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2022-02-02 15:21:57 +0100

    Fixed issue with conference factory URI being overriden when account is created from RC

--- src/account/account-params.cpp
+++ src/account/account-params.cpp
@@ -177,7 +177,7 @@ AccountParams::AccountParams (LinphoneCore *lc, int index) : AccountParams(lc) {
 		mNatPolicy = linphone_core_create_nat_policy_from_config(lc, nat_policy_ref);
 	}
 
-	mConferenceFactoryUri = linphone_config_get_string(config, key, "conference_factory_uri", mConferenceFactoryUri.c_str());
+	mConferenceFactoryUri = linphone_config_get_string(config, key, "conference_factory_uri", "");
 }
 
 AccountParams::AccountParams (const AccountParams &other) : HybridObject(other) {
