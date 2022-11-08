00546655e683d937349549c09dc00519290adfa0
Mickaël Turnel <mickael.turnel@belledonne-communications.com>
2022-11-08 10:44:12 +0100

    Fix active speaker call back not called when not receiving any video

--- src/conference/session/ms2-streams.h
+++ src/conference/session/ms2-streams.h
@@ -301,6 +301,8 @@ public:
 	void setVideoSource (const std::shared_ptr<const VideoSourceDescriptor> &descriptor);
 	std::shared_ptr<const VideoSourceDescriptor> getVideoSource () const;
 
+	static void sCsrcChangedCb (void *userData, uint32_t new_csrc);
+
 	virtual ~MS2VideoStream();
 protected:
 	AudioStream *getPeerAudioStream();
@@ -314,7 +316,6 @@ private:
 	static void sVideoStreamEventCb (void *userData, const MSFilter *f, const unsigned int eventId, const void *args);
 	void cameraNotWorkingCb (const char *cameraName);
 	static void sCameraNotWorkingCb (void *userData, const MSWebCam *oldWebcam);
-	static void sCsrcChangedCb (void *userData, uint32_t new_csrc);
 	MS2VideoMixer *getVideoMixer();
 	VideoStream *mStream = nullptr;
 	struct _MSVideoEndpoint *mConferenceEndpoint = nullptr;
