5fcd74673d731ef707d8910bfde2e39d2d93bdb6
Simon Morlat <simon.morlat@linphone.org>
2023-10-13 17:59:48 +0200

    Enable deflate compression for x3dh server engine. Indeed, multipart content for IMDNs can be very large.

--- src/chat/encryption/lime-x3dh-server-engine.cpp
+++ src/chat/encryption/lime-x3dh-server-engine.cpp
@@ -99,6 +99,11 @@ LimeX3dhEncryptionServerEngine::processOutgoingMessage(const std::shared_ptr<Cha
 	finalContent.setContentType(internalContent->getContentType());
 	finalContent.getContentType().removeParameter("boundary");
 	finalContent.getContentType().addParameter("boundary", boundary);
+	if (linphone_core_content_encoding_supported(L_GET_C_BACK_PTR(chatRoom->getCore()), "deflate")) {
+		finalContent.setContentEncoding("deflate");
+	} else {
+		lWarning() << "Cannot use 'deflate' Content-Encoding to compress body - consider rebuilding with libz support.";
+	}
 	message->setInternalContent(finalContent);
 	return ChatMessageModifier::Result::Done;
 }
