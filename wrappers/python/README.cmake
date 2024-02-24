f40a70e05be3c683016962aa8dca60ee239ae43f
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2024-02-24 11:27:20 +0100

    Improved generated doc for python wrapper

--- wrappers/python/README.cmake
+++ wrappers/python/README.cmake
@@ -45,16 +45,48 @@ http://www.linphone.org
 
 Here's how to import our module and print the liblinphone version that you are using:
 ```python
-from pylinphone import linphone
+import linphone
 print(linphone.Core.get_version())
 ```
 
-## Troubleshooting
+# Quick tour of liblinphone's features
 
-If you encounter an issue about grammar not being found, and that you installed our module using the wheel, here's how to fix it:
-```python
-import os
-from pylinphone import linphone
+## Introduction
 
-linphone.Factory.get().top_resources_dir = os.path.dirname(os.path.abspath(linphone.__file__)) + "/share/" 
-```
+Liblinphone's has a consistent object-oriented design.
+Root objects must be constructed by the `Factory` class. No 'new' operator invocation is permitted.
+Liblinphone is using SIP as signaling protocol, which actually comprises a huge set of RFCs to cover various aspects of communications. Some terminology of the API is directly inherited from
+SIP specifications, that's why having some knowledge of the protocol is recommended for a better understanding of this documentation.
+
+## Initializing the engine
+A typical liblinphone application has to first instanciate a `Core` object using the `Factory`. The core object represents the liblinphone engine, from which call, conferences, instant messages can be sent or received.
+For events to be reported and engine to schedule its tasks, the application must call `Core.iterate()` at regular interval, typically from a 20ms timer.
+In most case, a SIP account has to be provisionned so that SIP registration can take place onto a SIP server. This task is designated to the Account class.
+An Account can be created using `Core.create_account()`, based on parameters created with `Core.create_account_params()`.
+Then, account can be added to the core for usage using `Core.add_account()`.
+Application usually need to get informed of events occuring in the lifetime of the engine, which is done through listeners that the applications can override.
+An important listener interface is the `CoreListener`, that application should create, and then assign into their
+`Core` object through `Core.add_listener()` method.
+
+## Making calls
+Applications can place outgoing calls using `Core.invite()` or `Core.invite_address_with_params()`.
+The `CallParams` class represents parameters for the calls, such as enabling video, requiring a specific `MediaEncryption`.
+The `CallListener` interface provides application way to get inform of the progress of the call, represented by the `CallState` enum.
+Incoming calls are notified through the `CoreListener` interface, and can later be accepted using `Call.accept()` .
+Calls can be terminated or aborted at any time using `Call.terminate()` .
+
+## Instant messaging
+
+The `ChatRoom` class represents a text conversation. The `Core` object provides persistancy for all conversations, ie it stores all received and sent messages.
+The list of conversation can be retrieved using `Core.chat_rooms` property.
+To create a new conversation, use `Core.create_chat_room()`. `ChatRoomParams` provide a way to specify which kind of chatroom is to be created: for group,
+for one-ton-one conversation, with end-to end encryption for example.
+To send a message, first create the `ChatMessage` with `ChatRoom.create_message_from_utf8()`, then send it with `ChatMessage.send()` .
+A `ChatMessage` reports its progress through the `ChatMessageListener` interface.
+ChatRooms are automatically created by the `Core` when receiving a message that starts a new conversation, and notified through the `CoreListener` interface.
+
+## Presence
+
+Applications can submit presence information through the Core's `Core.presence_model` property. The `PresenceModel` class represents the presence information, which is submitted to a presence server.
+Symmetrically, applications can subscribe to the presence server to get notified of the presence status of a contact list.
+This is to be done thanks to the `FriendList` and `Friend` classes.
