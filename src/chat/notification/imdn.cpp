95143e2ba989e9b7dd723e7861e6308fb7f794b9
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2025-10-10 09:38:02 +0200

    Catch exception when destroying the IMDN objects

--- src/chat/notification/imdn.cpp
+++ src/chat/notification/imdn.cpp
@@ -50,8 +50,8 @@ Imdn::Imdn(ChatRoom *chatRoom) : chatRoom(chatRoom) {
 }
 
 Imdn::~Imdn() {
-	stopTimer();
 	try { // getCore may no longuer be available when deleting, specially in case of managed enviroment like java
+		stopTimer();
 		chatRoom->getCore()->getPrivate()->unregisterListener(this);
 	} catch (const bad_weak_ptr &) {
 	}
