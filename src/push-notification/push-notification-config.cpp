cad76708f393a419d7ccb6e3c87213b5b7e33f28
QuentinA <quentin.arguillere@belledone-communications.com>
2025-12-04 11:51:53 +0100

    When generating push parameters, re-generate new params if voipPushAllowed or remotePushAllowed have changed compared to the old parameters

--- src/push-notification/push-notification-config.cpp
+++ src/push-notification/push-notification-config.cpp
@@ -177,6 +177,18 @@ void PushNotificationConfig::setRemotePushInterval(const string &remotePushInter
 	mPushParams[PushConfigRemotePushIntervalKey] = remotePushInterval;
 }
 
+bool doesParamNeedUpdate(string const &paramVal, bool voipPushAllowed, bool remotePushAllowed) {
+	bool foundVoipInParam = paramVal.find("voip") != string::npos;
+	if (voipPushAllowed && !foundVoipInParam) return true;
+	if (!voipPushAllowed && foundVoipInParam) return true;
+
+	bool foundRemoteInParam = paramVal.find("remote") != string::npos;
+	if (remotePushAllowed && !foundRemoteInParam) return true;
+	if (!remotePushAllowed && foundRemoteInParam) return true;
+
+	return false;
+}
+
 void PushNotificationConfig::generatePushParams(bool voipPushAllowed, bool remotePushAllowed) {
 	if (mPushParams[PushConfigProviderKey].empty()) {
 #ifdef __ANDROID__
@@ -192,6 +204,7 @@ void PushNotificationConfig::generatePushParams(bool voipPushAllowed, bool remot
 	if (mVoipToken.empty()) voipPushAllowed = false;
 
 	if (mPushParams[PushConfigParamKey].empty() ||
+	    doesParamNeedUpdate(mPushParams[PushConfigParamKey], voipPushAllowed, remotePushAllowed) ||
 	    (mTokensHaveChanged && (!mVoipToken.empty() || !mRemoteToken.empty()))) {
 		string services;
 		if (voipPushAllowed) {
@@ -204,6 +217,7 @@ void PushNotificationConfig::generatePushParams(bool voipPushAllowed, bool remot
 	}
 
 	if (mPushParams[PushConfigPridKey].empty() ||
+	    doesParamNeedUpdate(mPushParams[PushConfigPridKey], voipPushAllowed, remotePushAllowed) ||
 	    (mTokensHaveChanged && (!mVoipToken.empty() || !mRemoteToken.empty()))) {
 		string newPrid;
 		if (voipPushAllowed) {
