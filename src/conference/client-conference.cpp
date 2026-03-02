f1c4b4825892aebfc1ed5b8efd152822f754fcea
Simon Morlat <simon.morlat@linphone.org>
2026-03-02 10:47:52 +0100

    Fix issue where a conference or chatroom subject cannot be changed back to its original value, because mPendingSubject is never reset.

--- src/conference/client-conference.cpp
+++ src/conference/client-conference.cpp
@@ -508,7 +508,7 @@ void ClientConference::setUtf8Subject(const std::string &subject) {
 	}
 	auto session = dynamic_pointer_cast<MediaSession>(getMainSession());
 	if (session) {
-		if ((subject.compare(mPendingSubject) != 0) || (getUtf8Subject().empty() && !subject.empty())) {
+		if ((subject != mPendingSubject) || (getUtf8Subject().empty() && !subject.empty())) {
 			mPendingSubject = subject;
 			auto updateSubject = [this, subject]() -> LinphoneStatus {
 				auto session = dynamic_pointer_cast<MediaSession>(getMainSession());
@@ -521,6 +521,8 @@ void ClientConference::setUtf8Subject(const std::string &subject) {
 					delete currentParams;
 					if (ret != 0) {
 						lInfo() << "re-INVITE to update subject to \"" << subject << "\" cannot be sent right now";
+					} else {
+						mPendingSubject.clear();
 					}
 					return ret;
 				}
@@ -537,6 +539,7 @@ void ClientConference::setUtf8Subject(const std::string &subject) {
 		session = dynamic_pointer_cast<MediaSession>(createSession());
 		if (session) {
 			session->startInvite(nullptr, subject, nullptr);
+			mPendingSubject.clear();
 		}
 	} else {
 		mPendingSubject = subject;
@@ -1270,7 +1273,7 @@ void ClientConference::onStateChanged(ConferenceInterface::State state) {
 			}
 			break;
 		case ConferenceInterface::State::Created:
-			if (session && getMe()->isAdmin() && !mPendingSubject.empty() && (subject.compare(mPendingSubject) != 0)) {
+			if (session && getMe()->isAdmin() && !mPendingSubject.empty() && (subject != mPendingSubject)) {
 				setUtf8Subject(mPendingSubject);
 			}
 			break;
